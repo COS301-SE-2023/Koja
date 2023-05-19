c67a5430038cfc1a3cd86d30e310786cf5a3cc15
UnarineLeo <unarineleo@gmail.com>
2023-05-19 15:57:26 +0200

    location & push testing

--- client/src/widgets/settings_list_widget.dart
+++ client/src/widgets/settings_list_widget.dart
@@ -13,6 +13,10 @@ class settingslist extends StatefulWidget {
 
 class _settingslistState extends State<settingslist> {
   bool _customTileExpanded = false;
+  static const div = Divider(
+    thickness: 0,
+    height: 2,
+  );
 
   @override
   Widget build(BuildContext context) {
@@ -30,8 +34,7 @@ class _settingslistState extends State<settingslist> {
                 shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(10),
                 ),
-                leading: Icon(Icons.watch_sharp,
-                    color: Colors.white, size: 30),
+                leading: Icon(Icons.watch_sharp, color: Colors.white, size: 30),
                 title: const Text('Active Times',
                     style: TextStyle(
                       color: Colors.white,
@@ -44,99 +47,102 @@ class _settingslistState extends State<settingslist> {
                   Icons.arrow_drop_down,
                   color: Colors.white,
                 ),
-                
                 children: [
-                    Divider(color: Colors.white, 
-                    thickness: 1, 
+                  Divider(
+                    color: Colors.white,
+                    thickness: 1,
                     height: 1,
-                    ),
-                    SizedBox( height: 15),
-                    Row(
-                      children: [
-                        Container(
-                          height: 120,
-                          //Work Time Column
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            
-                            children: [
-                              Text("Work Time:",
-                                style: TextStyle(
-                                  color: Colors.white,
-                                ),
+                  ),
+                  SizedBox(height: 15),
+                  Row(
+                    children: [
+                      Container(
+                        height: 120,
+                        //Work Time Column
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Text(
+                              "Work Time:",
+                              style: TextStyle(
+                                color: Colors.white,
                               ),
-                              Row(
-                                  
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: [
-                                    SizedBox(width: 10),
-                                    Text('Start Time :',
-                                    style: TextStyle(
-                                      color: Colors.white,
-                                    ),),
-                                    SizedBox(width: 10), 
-                                    TimePickerWidget(),
-                                  ],
-                                ),
-                                Row(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: [
-                                    SizedBox(width: 10),
-                                    Text('End Time   :',
-                                    style: TextStyle(
-                                      color: Colors.white,
-                                    ),
-                                    ),
-                                    SizedBox(width: 10), 
-                                    TimePickerWidget(),
-                                  ],
-                                ),          
-                            ],
-                          ),
-                        ),
-                        SizedBox(width: 20),
-                        Container(
-                          height: 120,
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            children: [                          
-                                Text("Personal Time:",
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                SizedBox(width: 10),
+                                Text(
+                                  'Start Time :',
                                   style: TextStyle(
                                     color: Colors.white,
                                   ),
                                 ),
-                              Row(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: [
-                                    Text('Start Time :',
-                                    style: TextStyle(
-                                      color: Colors.white,
-                                    ),
-                                    ),
-                                    SizedBox(width: 10), 
-                                    TimePickerWidget(),
-                                  ],
+                                SizedBox(width: 10),
+                                TimePickerWidget(),
+                              ],
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                SizedBox(width: 10),
+                                Text(
+                                  'End Time   :',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                  ),
                                 ),
-                                Row(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: [
-                                    Text('End Time   :',
-                                    style: TextStyle(
-                                      color: Colors.white,
-                                    ),
-                                    ),
-                                    SizedBox(width: 10), 
-                                    TimePickerWidget(),
-                                  ],
-                                ),   
-                            ],
-                          ),
+                                SizedBox(width: 10),
+                                TimePickerWidget(),
+                              ],
+                            ),
+                          ],
                         ),
-                      ],
-                    ),
-                    
-                  
+                      ),
+                      SizedBox(width: 20),
+                      Container(
+                        height: 120,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Text(
+                              "Personal Time:",
+                              style: TextStyle(
+                                color: Colors.white,
+                              ),
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text(
+                                  'Start Time :',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                  ),
+                                ),
+                                SizedBox(width: 10),
+                                TimePickerWidget(),
+                              ],
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text(
+                                  'End Time   :',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                  ),
+                                ),
+                                SizedBox(width: 10),
+                                TimePickerWidget(),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
                 ],
                 onExpansionChanged: (bool expanded) {
                   setState(() {
@@ -152,18 +158,15 @@ class _settingslistState extends State<settingslist> {
             height: 10,
           ),
 
-
           //Second Expansion Tile[Location Picker]
           SingleChildScrollView(
             padding: EdgeInsets.all(0),
-            
             child: Container(
               width: MediaQuery.of(context).size.width * 0.9,
               color: ThemeData.light().primaryColor,
               child: ExpansionTile(
                 leading: Icon(Icons.location_city_outlined,
-                    color: Colors.white, size: 30
-                ),
+                    color: Colors.white, size: 30),
                 title: const Text('Location',
                     style: TextStyle(
                       color: Colors.white,
@@ -176,14 +179,33 @@ class _settingslistState extends State<settingslist> {
                   Icons.arrow_drop_down,
                   color: Colors.white,
                 ),
+                
                 children: [
+                  Divider(
+                    color: Colors.white,
+                    thickness: 1,
+                    height: 1,
+                  ),
                   Container(
                     color: Colors.blue,
                     height: 100,
+                    child: Text(
+                      "Work Location",
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
                   ),
                   Container(
                     color: Colors.red,
                     height: 100,
+                    width: MediaQuery.of(context).size.width * 1,
+                    child: Text(
+                      "Home Location",
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
                   ),
                 ],
                 onExpansionChanged: (bool expanded) {
