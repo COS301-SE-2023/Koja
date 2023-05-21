745ea66ce334afb1904fa8f8e11fb15c9c4f943f
UnarineLeo <unarineleo@gmail.com>
2023-05-21 13:32:32 +0200

    calendar fix

--- client/src/widgets/location_search_widget.dart
+++ client/src/widgets/location_search_widget.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
-import '';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:line_icons/line_icons.dart';
+import 'package:http/http.dart' as http;
+
+import '../Utils/constants_util.dart';
+import 'location_auto_complete.dart';
+import 'location_tile.dart';
 import 'settings_widget.dart';
 
 class LocationSearch extends StatefulWidget {
@@ -10,7 +16,26 @@ class LocationSearch extends StatefulWidget {
 }
 
 class _LocationSearchState extends State<LocationSearch> {
-  
+
+  List<Predictions> predictions = [];
+
+  void placeAutocomplete(String query) async {
+    Uri uri = Uri.https(
+        "maps.googleapis.com",
+        "maps.googleapis.com/maps/api/place/autocomplete/json",
+        {"input": query, "key": apiKey});
+    String? response = await NetworkUtility.fetchUrl(uri);
+
+    if(response != null) {
+      PlaceAutocompleteResponse results = PlaceAutocompleteResponse.parseAutocompleteResult(response);
+      if(results.status == "OK") {
+        setState(() {
+          predictions = results.predictions!;
+        });
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,7 +43,80 @@ class _LocationSearchState extends State<LocationSearch> {
         title: const Text('Find a location',
             style: TextStyle(color: Colors.white)),
       ),
-      body: 
+      body: Column(
+        children: [
+          const SizedBox(height: 10),
+          Form(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextFormField(
+                onChanged: (value) {
+                  placeAutocomplete(value);
+                },
+                decoration: InputDecoration(
+                  hintText: 'Enter a location',
+                  suffixIcon: IconButton(
+                    icon: const Icon(Icons.search),
+                    onPressed: () {},
+                  ), 
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(height: 10),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ElevatedButton.icon(
+              icon: const Icon(LineIcons.mapMarker),
+              label: Text(
+                'Use current location',
+                style: GoogleFonts.lato(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w300,
+                  color: Colors.white,
+                ),
+              ),
+              onPressed: () {
+                
+              },
+              style: ElevatedButton.styleFrom(
+                foregroundColor: Colors.white,
+                backgroundColor: Colors.blue,
+                minimumSize: const Size(double.infinity, 50),
+              ),
+            ),
+          ),
+          Divider(color: Colors.grey[400], thickness: 1, height: 4),
+          Expanded(
+            child: ListView.builder(
+              itemCount: predictions.length,
+              itemBuilder: (BuildContext context, int index) { 
+                  LocationListTile(
+                    press: () {},
+                    location: predictions[index].description!,
+                    key: Key('Hatfield, Pretoria, South Africa'),
+                  );
+               },
+          
+            ),
+          ),
+        ],
+      ),
     );
   }
+}
+
+class NetworkUtility {
+  static Future<String?> fetchUrl(Uri uri, {Map<String, String>? headers}) async {
+    try {
+      final response = await http.get(uri, headers: headers);
+      if (response.statusCode == 200) {
+        return response.body;
+      } 
+    } catch (e) {
+      debugPrint(e.toString());
+    }
+    return null;
+  }
+  
 }
--- client/src/widgets/tasks_widget.dart
+++ client/src/widgets/tasks_widget.dart
@@ -3,7 +3,9 @@ import 'package:provider/provider.dart';
 import 'package:syncfusion_flutter_calendar/calendar.dart';
 import 'package:syncfusion_flutter_core/theme.dart';
 
+import '../Utils/constants_util.dart';
 import '../Utils/event_data_source_util.dart';
+import 'event_editing_widget.dart';
 import 'event_provider.dart';
 import 'event_viewing_widget.dart';
 
@@ -59,47 +61,45 @@ class _TasksWidgetState extends State<TasksWidget> {
           // border: Border.all(color: Colors.black, width: 2),
           // borderRadius: BorderRadius.circular(12),
         ),
-        onTap: (calendarTapDetails) {
-          if (calendarTapDetails.appointments == null) {
-           return;
+        onTap: (details) {
+          if (details.appointments == null) {
+            return;
           }
 
-          final event = calendarTapDetails.appointments!.first;
+          final event = details.appointments!.first;
           Navigator.push(
-            context, 
+            context,
             MaterialPageRoute(
-              builder: (context) => EventViewing(event: event),
+              builder: (context) => EventEditing(
+                event: event,
+              ),
             ),
           );
-
         },
       ),
-
     );
-    
   }
 
   Widget appointmentBuilder(
-    BuildContext context, 
-    CalendarAppointmentDetails calendarAppointmentDetails) {
-      final event = calendarAppointmentDetails.appointments.first;
-      return Container(
-        width: calendarAppointmentDetails.bounds.width,
-        height: calendarAppointmentDetails.bounds.height,
-        decoration: BoxDecoration(
-          color: event.background,
-          borderRadius: BorderRadius.circular(12),
-        ),
-        child: Center(
-          child: Text(
-            event.title,
-            style: const TextStyle(
-              color: Colors.white,
-              fontSize: 16,
-              fontWeight: FontWeight.bold,
-            ),
+      BuildContext context, CalendarAppointmentDetails details) {
+    final event = details.appointments.first;
+    return Container(
+      width: details.bounds.width,
+      height: details.bounds.height,
+      decoration: BoxDecoration(
+        color: darkBlue,
+        borderRadius: BorderRadius.circular(12),
+      ),
+      child: Center(
+        child: Text(
+          event.title,
+          style: const TextStyle(
+            color: Colors.white,
+            fontSize: 16,
+            fontWeight: FontWeight.bold,
           ),
         ),
-      );
+      ),
+    );
   }
 }
