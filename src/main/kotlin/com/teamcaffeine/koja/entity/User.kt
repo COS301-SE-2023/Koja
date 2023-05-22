6795f0ecbd408e34413ce748604c892f89e3c79c
U18034332 <U18034332@tuks.co.za>
2023-05-22 09:14:05 +0200

    Updated user

--- src/main/kotlin/com/teamcaffeine/koja/entity/User.kt
+++ src/main/kotlin/com/teamcaffeine/koja/entity/User.kt
@@ -2,7 +2,13 @@ package com.teamcaffeine.koja.entity
 
 import jakarta.persistence.Entity
 import jakarta.persistence.Id
+import jakarta.persistence.Table
+import java.time.LocalDateTime
+import java.util.*
 
 @Entity
-data class User(@Id val userID : String?,val userName : String) {
+data class User(@Id private val userID : String?, private val userEmail : String,
+                private val startactiveTime : LocalDateTime,
+                private val endactiveTime : LocalDateTime,
+                private val locations : String, private val calendar: Calendar) {
 }
