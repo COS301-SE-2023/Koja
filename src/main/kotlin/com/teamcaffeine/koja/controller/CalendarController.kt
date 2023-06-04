cf1460d9d22bf7504efa34af1ef6bca7fdfacc60
Marthinus Ettienne van Zyl <u19012366@tuks.co.za>
2023-06-04 02:53:28 +0200

    Add base JWT scaffolding

--- src/main/kotlin/com/teamcaffeine/koja/controller/CalendarController.kt
+++ src/main/kotlin/com/teamcaffeine/koja/controller/CalendarController.kt
@@ -1,13 +1,17 @@
+
+import com.teamcaffeine.koja.entity.Event
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 
 @RestController
+@RequestMapping("/api/v1/user/calendar")
 class CalendarController(private val userCalendar: UserCalendar) {
     @GetMapping("/events")
     fun getAllUserEvents(@PathVariable userId: String?): ResponseEntity<List<Event>> {
-        return ResponseEntity.ok(userCalendar.getAllEvents(userId))
+        return ResponseEntity.ok(userCalendar.events)
     }
 }
