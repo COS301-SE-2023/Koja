083dc11c760d6c3c5987c94cf5ed280b8e6ff136
UnarineLeo <unarineleo@gmail.com>
2023-06-08 22:00:41 +0200

    time boundary - no styling and overflow error

--- client/lib/widgets/time_boundaries_widget.dart
+++ client/lib/widgets/time_boundaries_widget.dart
@@ -2,6 +2,7 @@ import 'package:client/widgets/set_boundary_widget.dart';
 import 'package:client/widgets/time_category_list.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:http/http.dart';
 
 class TimeBoundaries extends StatefulWidget {
   @override
@@ -29,7 +30,13 @@ class _TimeBoundariesState extends State<TimeBoundaries> {
   // list of categories
   List categories = [];
 
+  // index of list item which is being edited so that we can delete the current item and add the edited item
+  int editedindex = -1;
+
+  // selected option for dropdown
   late String selectedOption;
+
+  // list of options for dropdown
   List<String> dropdownOptions = [
     'School',
     'Work',
@@ -38,20 +45,30 @@ class _TimeBoundariesState extends State<TimeBoundaries> {
     'Chores',
   ];
 
+  // function to save time
   void saveTime() {
-    print("Saving time");
-    print(_start.text);
-    print(_end.text);
-    print(selectedOption);
     setState(() {
       categories.add([selectedOption, _start.text, _end.text]);
+    });
+
+    //if editing an item, remove the current item from the list and add the edited item
+    if (editedindex != -1) {
+      categories.removeAt(editedindex);
+      editedindex = 0;
+    }
+  }
+
+  // function to delete an item from the list
+  void delete(int index) {
+    setState(() {
+      categories.removeAt(index);
     });
   }
 
   @override
   void initState() {
     super.initState();
-    selectedOption = dropdownOptions[0]; 
+    selectedOption = dropdownOptions[0];
   }
 
   @override
@@ -64,8 +81,6 @@ class _TimeBoundariesState extends State<TimeBoundaries> {
       title: Text("Click To Set Your Active Times ",
           style: GoogleFonts.ubuntu(
             fontSize: 15,
-            // fontWeight: FontWeight.bold,
-            // color: Colors.white,
           )),
       children: [
         Padding(
@@ -105,7 +120,6 @@ class _TimeBoundariesState extends State<TimeBoundaries> {
                 child: IconButton(
                   icon: Icon(Icons.add),
                   onPressed: () {
-                    print('Add button pressed!' + selectedOption);
                     showDialog(
                         context: context,
                         builder: (context) {
@@ -119,19 +133,25 @@ class _TimeBoundariesState extends State<TimeBoundaries> {
           ),
         ),
         SizedBox(height: 10),
-        // TimeCategory("selectedOption", "_start", "_end"),
         SizedBox(
           height: 400,
           child: ListView.builder(
             itemCount: categories.length,
             itemBuilder: (context, index) {
               return Container(
-                height: 90,
-                child: TimeCategory(
-                    categories[index][0], 
-                    categories[index][1], 
-                    categories[index][2]
-                ),
+                height: 100,
+                child: TimeCategory(categories[index][0], categories[index][1],
+                    categories[index][2], (context) {
+                  delete(index);
+                }, (context) {
+                  editedindex = index;
+                  showDialog(
+                      context: context,
+                      builder: (context) {
+                        return SetBoundary(
+                            categories[index][0], _start, _end, saveTime);
+                      });
+                }),
               );
             },
           ),
