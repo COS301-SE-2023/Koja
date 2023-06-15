29d08e19ebff57bb2e36f9f73610fc567d6a562d
Marthinus Ettienne van Zyl <u19012366@tuks.co.za>
2023-06-15 14:45:41 +0200

    Fix token expire dates

--- src/main/kotlin/com/teamcaffeine/koja/controller/CalendarController.kt
+++ src/main/kotlin/com/teamcaffeine/koja/controller/CalendarController.kt
@@ -1,10 +1,11 @@
 package com.teamcaffeine.koja.controller
 
+import com.teamcaffeine.koja.entity.GoogleCalendarAdapter
 import com.teamcaffeine.koja.entity.UserCalendar
 import com.teamcaffeine.koja.entity.UserEvent
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -12,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/api/v1/user/calendar")
 class CalendarController(private val userCalendar: UserCalendar) {
     @GetMapping("/userEvents")
-    fun getAllUserEvents(@PathVariable userId: String?): ResponseEntity<List<UserEvent>> {
-        return ResponseEntity.ok(userCalendar.userEvents)
+    fun getAllUserEvents(@RequestBody token: String): ResponseEntity<List<UserEvent>> {
+        return ResponseEntity.ok(GoogleCalendarAdapter().getUserEvents(token))
     }
 }
--- src/main/kotlin/com/teamcaffeine/koja/entity/UserCalendar.kt
+++ src/main/kotlin/com/teamcaffeine/koja/entity/UserCalendar.kt
@@ -19,12 +19,6 @@ class UserCalendar() {
     public val issues: ArrayList<Issue> = ArrayList();
 
 
-//    public fun createEvent(title: String, start: DateTime, end: DateTime): UserEvent{
-//
-//        return UserEvent(title,start,end);
-//
-//    }
-
     fun addCalendarAdapter(adapter: CalendarAdapter){
         this.calendarAdapters.add(adapter);
         for( event in adapter.getEvents()!!)
