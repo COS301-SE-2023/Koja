74e94a3f9f8c314bbb548ec9d24f7ca666e60302
Marthinus Ettienne van Zyl <u19012366@tuks.co.za>
2023-06-15 11:13:18 +0200

    Refactor some code segments

--- src/main/kotlin/com/teamcaffeine/koja/controller/TokenManagerController.kt
+++ src/main/kotlin/com/teamcaffeine/koja/controller/TokenManagerController.kt
@@ -6,6 +6,7 @@ import com.auth0.jwt.exceptions.JWTDecodeException
 import com.auth0.jwt.exceptions.JWTVerificationException
 import com.auth0.jwt.interfaces.DecodedJWT
 import com.teamcaffeine.koja.enums.AuthProviderEnum
+import com.teamcaffeine.koja.enums.JWTTokenStructure
 import com.teamcaffeine.koja.enums.getAuthProvider
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -32,11 +33,11 @@ class TokenManagerController {
             val decodedJWT: DecodedJWT = verifier.verify(refreshToken)
 
             // TODO: Use refresh token to get new access token from Auth Provider and update expiry time
-            val newAccessToken = decodedJWT.getClaim("access_token").asString()
-            val newExpiryTime = decodedJWT.getClaim("expiry_time").asLong()
-            val newRefreshToken = decodedJWT.getClaim("refresh_token").asString()
-            val authProvider = decodedJWT.getClaim("auth_provider").asString()
-            val userID = decodedJWT.getClaim("user_id").asLong()
+            val newAccessToken = decodedJWT.getClaim(JWTTokenStructure.ACCESS_TOKEN.claimName).asString()
+            val newExpiryTime = decodedJWT.getClaim(JWTTokenStructure.EXPIRES_TIME.claimName).asDate().time
+            val newRefreshToken = decodedJWT.getClaim(JWTTokenStructure.REFRESH_TOKEN.claimName).asString()
+            val authProvider = decodedJWT.getClaim(JWTTokenStructure.AUTH_PROVIDER.claimName).asString()
+            val userID = decodedJWT.getClaim(JWTTokenStructure.USER_ID.claimName).asLong()
 
             return createJwtToken(newAccessToken, newExpiryTime, newRefreshToken, getAuthProvider(authProvider), userID)
         } catch (e: JWTDecodeException) {
@@ -63,11 +64,11 @@ class TokenManagerController {
         val algorithm = Algorithm.HMAC512(jwtSecret)
 
         return JWT.create()
-            .withClaim("access_token", accessToken)
-            .withClaim("expiry_time", expiryTime)
-            .withClaim("refresh_token", refreshToken)
-            .withClaim("auth_provider", authProvider.toString())
-            .withClaim("user_id", userID)
+            .withClaim(JWTTokenStructure.ACCESS_TOKEN.claimName, accessToken)
+            .withClaim(JWTTokenStructure.EXPIRES_TIME.claimName, expiryTime)
+            .withClaim(JWTTokenStructure.REFRESH_TOKEN.claimName, refreshToken)
+            .withClaim(JWTTokenStructure.AUTH_PROVIDER.claimName, authProvider.toString())
+            .withClaim(JWTTokenStructure.USER_ID.claimName, userID)
             .withExpiresAt(Date(System.currentTimeMillis() + expiryTime))
             .sign(algorithm)
     }
