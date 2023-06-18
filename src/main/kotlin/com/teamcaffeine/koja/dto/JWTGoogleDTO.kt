8fd1422f45d2e4b1d5c70d00b95b605cb30a2409
Marthinus Ettienne van Zyl <u19012366@tuks.co.za>
2023-06-18 18:51:49 +0200

    Update code to migrate to UserCalendarService

--- src/main/kotlin/com/teamcaffeine/koja/dto/JWTGoogleDTO.kt
+++ src/main/kotlin/com/teamcaffeine/koja/dto/JWTGoogleDTO.kt
@@ -1,15 +1,26 @@
 package com.teamcaffeine.koja.dto
 
-class JWTGoogleDTO(public val accessToken: String, public val refreshToken: String, public val expireTimeInSeconds: Long) : JWTAuthDetailsDTO {
+import com.teamcaffeine.koja.enums.AuthProviderEnum
+
+class JWTGoogleDTO(private val accessToken: String, private val refreshToken: String, public val expireTimeInSeconds: Long) : JWTAuthDetailsDTO() {
     override fun getJWTFormat(): String {
         return mapOf(
             "googleAccessToken" to accessToken,
             "googleRefreshToken" to refreshToken,
-            "googleExpireDate" to expireTimeInSeconds
+            "googleExpireDate" to expireTimeInSeconds,
+            "provider" to AuthProviderEnum.GOOGLE.name
         ).toString()
     }
 
     override fun getExpireTime(): Long {
         return expireTimeInSeconds
+    }
+
+    override fun getRefreshToken(): String {
+        return refreshToken
+    }
+
+    override fun getAccessToken(): String {
+        return accessToken
     }
 }
--- src/main/kotlin/com/teamcaffeine/koja/service/UserCalendarService.kt
+++ src/main/kotlin/com/teamcaffeine/koja/service/UserCalendarService.kt
@@ -1,8 +1,7 @@
 package com.teamcaffeine.koja.service
 
-import com.teamcaffeine.koja.controller.TokenManagerController.Companion.decodeJwtToken
+import com.teamcaffeine.koja.controller.TokenManagerController.Companion.getUserJWTTokenData
 import com.teamcaffeine.koja.dto.UserEventDTO
-import com.teamcaffeine.koja.enums.JWTTokenStructureEnum
 import com.teamcaffeine.koja.repository.UserAccountRepository
 import com.teamcaffeine.koja.repository.UserRepository
 import org.springframework.stereotype.Service
@@ -11,23 +10,26 @@ import org.springframework.stereotype.Service
 class UserCalendarService(private val userAccountRepository: UserAccountRepository, private val userRepository: UserRepository) {
 
     public fun getAllUserEvents(token: String): List<UserEventDTO> {
-        val decodedJWT = decodeJwtToken(token)
-        val userID = decodedJWT.getClaim(JWTTokenStructureEnum.USER_ID.claimName).asInt()
-        val userAccounts = userAccountRepository.findByUserID(userID)
+        val userJWTTokenData = getUserJWTTokenData(token)
 
+        val userAccounts = userAccountRepository.findByUserID(userJWTTokenData.userID)
         val calendarAdapters = ArrayList<CalendarAdapterService>()
         val adapterFactory = CalendarAdapterFactoryService( userRepository, userAccountRepository)
 
         for (ua in userAccounts) {
-            val adapter = adapterFactory.createCalendarAdapter(ua.authProvider)
             calendarAdapters.add(adapterFactory.createCalendarAdapter(ua.authProvider))
         }
 
         val userEvents = ArrayList<UserEventDTO>()
 
-        for (ca in calendarAdapters) {
-            val events = ca.getUserEvents(token)
-            userEvents.addAll(events)
+        for (adapter in calendarAdapters) {
+            val userAccount = userAccounts[calendarAdapters.indexOf(adapter)]
+            val accessToken =
+                userJWTTokenData.userAuthDetails.firstOrNull() { it.getRefreshToken() == userAccount.refreshToken }
+                    ?.getAccessToken()
+            if (accessToken != null) {
+                userEvents.addAll(adapter.getUserEvents(accessToken))
+            }
         }
 
         return userEvents
