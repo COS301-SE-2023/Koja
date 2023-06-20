2d7a1cc483cabc952867035f9ef0c84fe9e04a31
Elias Makhonjwa <u18034332@tuks.co.za>
2023-06-20 19:33:35 +0200

    JWTGoogleDTO unit Test

--- src/test/kotlin/com/teamcaffiene/koja/dto/JWTGoogleDTOTest.kt
+++ src/test/kotlin/com/teamcaffiene/koja/dto/JWTGoogleDTOTest.kt
@@ -1,4 +1,17 @@
 package com.teamcaffiene.koja.dto
 
+import com.teamcaffeine.koja.dto.JWTGoogleDTO
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
 class JWTGoogleDTOTest {
+
+    private val jwtGoogleDTO: JWTGoogleDTO = JWTGoogleDTO("12345accessToken","1234refResHtOken",1L)
+
+    @Test
+    fun `CreateJWTGoogleDTO_SUCCESS`(){
+        assertEquals("12345accessToken",jwtGoogleDTO.getAccessToken())
+        assertEquals("1234refResHtOken",jwtGoogleDTO.getRefreshToken())
+        assertEquals(1L,jwtGoogleDTO.getExpireTime())
+    }
 }
