f9d87c88c35961e6b754e48cc1e9b0b39fd46d4f
Elias Makhonjwa <u18034332@tuks.co.za>
2023-06-22 16:10:27 +0200

    CalendarController Integration Test

--- src/test/kotlin/com/teamcaffiene/koja/controller/AuthenticationControllerUnitTest.kt
+++ src/test/kotlin/com/teamcaffiene/koja/controller/AuthenticationControllerUnitTest.kt
@@ -14,7 +14,7 @@ import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.servlet.view.RedirectView
 
-class AuthenticationControllerTest {
+class AuthenticationControllerUnitTest {
     @Mock
     private lateinit var googleCalendarAdapterService: GoogleCalendarAdapterService
 
--- src/test/kotlin/com/teamcaffiene/koja/controller/CalendarControllerUnitTest.kt
+++ src/test/kotlin/com/teamcaffiene/koja/controller/CalendarControllerUnitTest.kt
@@ -14,7 +14,7 @@ import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import java.util.*
 
-class CalendarControllerTest {
+class CalendarControllerUnitTest {
     @Mock
     private lateinit var userCalendarService: UserCalendarService
 
