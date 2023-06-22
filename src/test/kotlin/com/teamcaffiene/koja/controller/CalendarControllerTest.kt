886931f656668f7bd8142693ec05b6ea19c24b51
Elias Makhonjwa <u18034332@tuks.co.za>
2023-06-22 14:44:56 +0200

    CalendarController unit Test

--- src/test/kotlin/com/teamcaffiene/koja/controller/CalendarControllerTest.kt
+++ src/test/kotlin/com/teamcaffiene/koja/controller/CalendarControllerTest.kt
@@ -1,4 +1,48 @@
 package com.teamcaffiene.koja.controller
 
+import com.teamcaffeine.koja.controller.CalendarController
+import com.teamcaffeine.koja.dto.UserEventDTO
+import com.teamcaffeine.koja.service.UserCalendarService
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.Mockito.`when`
+import org.mockito.MockitoAnnotations
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import java.util.*
+
 class CalendarControllerTest {
+    @Mock
+    private lateinit var userCalendarService: UserCalendarService
+
+    @InjectMocks
+    private lateinit var calendarController: CalendarController
+
+    @BeforeEach
+    fun setup() {
+        MockitoAnnotations.openMocks(this)
+    }
+
+    @Test
+    fun testGetAllUserEvents() {
+        // Mock the behavior of the userCalendarService
+        val token = "testToken"
+        val userEvents = listOf(UserEventDTO(
+            "1", "5KM Morning Jog", "LC sports center", Date(2015, 5, 28, 7, 0), Date(2015, 5, 28, 9, 0)
+            )
+        )
+        `when`(userCalendarService.getAllUserEvents(token)).thenReturn(userEvents)
+
+        // Invoke the getAllUserEvents method in the calendarController
+        val responseEntity: ResponseEntity<List<UserEventDTO>> = calendarController.getAllUserEvents(token)
+
+        // Verify the result
+        assertEquals(HttpStatus.OK, responseEntity.statusCode)
+        assertEquals(userEvents, responseEntity.body)
+
+
+    }
 }
