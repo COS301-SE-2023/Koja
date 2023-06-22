e9eb47d4b40d6443bdec2b87e402dd23a0063d69
Marthinus Ettienne van Zyl <u19012366@tuks.co.za>
2023-06-22 22:00:00 +0200

    Attempt to fix test issues causing test failure

--- src/test/kotlin/com/teamcaffiene/koja/controller/AuthenticationControllerIntegrationTest.kt
+++ src/test/kotlin/com/teamcaffiene/koja/controller/AuthenticationControllerIntegrationTest.kt
@@ -5,44 +5,42 @@ import com.teamcaffeine.koja.service.GoogleCalendarAdapterService
 import org.junit.jupiter.api.Test
 import org.mockito.Mockito.`when`
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
-import org.springframework.test.context.junit.jupiter.SpringJUnitConfig
+import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.get
 
-@SpringJUnitConfig
 @SpringBootTest(classes = [KojaApplication::class])
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 class AuthenticationControllerIntegrationTest {
 
     @Autowired
     private lateinit var mockMvc: MockMvc
 
     @MockBean
+    @Qualifier("googleCalendarAdapterService")
     private lateinit var googleCalendarAdapter: GoogleCalendarAdapterService
 
     @Test
     fun testHandleGoogleOAuth2Callback() {
-        // Prepare test data
         val authCode = "test_auth_code"
-        val responseContent = "OAuth2 callback successful"
+        val responseContent = "test_token"
 
-        // Mock the service method
         `when`(googleCalendarAdapter.oauth2Callback(authCode)).thenReturn(ResponseEntity.ok(responseContent))
 
-        // Perform the GET request
         mockMvc.get("/api/v1/auth/google/callback") {
             param("code", authCode)
             accept(MediaType.APPLICATION_JSON)
         }.andExpect {
             status { isOk() }
             content {
-                contentType(MediaType.APPLICATION_JSON)
-                json(responseContent)
+                string(responseContent)
             }
         }
     }
