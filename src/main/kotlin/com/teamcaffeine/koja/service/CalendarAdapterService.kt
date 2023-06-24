790e4b0bc8f663f5a8900f9e1ae7f5e34f58a560
Marthinus Ettienne van Zyl <u19012366@tuks.co.za>
2023-06-24 18:35:03 +0200

    Update endpoints for app auth usage

--- src/main/kotlin/com/teamcaffeine/koja/service/CalendarAdapterService.kt
+++ src/main/kotlin/com/teamcaffeine/koja/service/CalendarAdapterService.kt
@@ -3,7 +3,6 @@ package com.teamcaffeine.koja.service
 import com.teamcaffeine.koja.dto.UserEventDTO
 import com.teamcaffeine.koja.enums.AuthProviderEnum
 import jakarta.servlet.http.HttpServletRequest
-import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Service
 import org.springframework.web.servlet.view.RedirectView
 
@@ -13,7 +12,7 @@ abstract class CalendarAdapterService(authProvider: AuthProviderEnum) {
 
     abstract fun setupConnection(request: HttpServletRequest?): RedirectView
     abstract fun authorize(): String?
-    abstract fun oauth2Callback(authCode: String?): ResponseEntity<String>
+    abstract fun oauth2Callback(authCode: String?): String
     abstract fun getUserEvents(accessToken: String): List<UserEventDTO>
 
     abstract fun getUserEmail(accessToken: String): String?
