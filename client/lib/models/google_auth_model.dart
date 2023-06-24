c818cf94746a47498f8c5a598f5e3f51e8ddd562
UnarineLeo <unarineleo@gmail.com>
2023-06-24 11:15:15 +0200

    upcoming events on homepage

--- client/lib/models/google_auth_model.dart
+++ client/lib/models/google_auth_model.dart
@@ -1,11 +1,29 @@
 import 'package:google_sign_in/google_sign_in.dart';
 
 class GoogleAuthModel {
-  static final GoogleSignIn _googleSignIn = GoogleSignIn();
+  static final GoogleSignIn _googleSignIn = GoogleSignIn(
+    // scopes: [
+    //   'email',
+    //   'https://www.googleapis.com/auth/contacts.readonly',
+    // ],
+  );
 
   static Future<GoogleSignInAccount?> login() async {
-    return await _googleSignIn.signIn();
+    try {
+      final GoogleSignInAccount? googleSignInAccount =
+          await _googleSignIn.signIn();
+      return googleSignInAccount;
+    } catch (error) {
+      print(error);
+      return null;
+    }
   }
 
-  // static Future logout() => _googleSignIn.signOut();
+  static Future<void> logout() async {
+    try {
+      await _googleSignIn.signOut();
+    } catch (error) {
+      print(error);
+    }
+  }
 }
