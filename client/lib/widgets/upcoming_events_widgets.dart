80fdee7f96c7443f37d994670be76b9702bef5f5
UnarineLeo <unarineleo@gmail.com>
2023-06-26 06:00:37 +0200

    update home page - task overview calculations

--- client/lib/widgets/upcoming_events_widgets.dart
+++ client/lib/widgets/upcoming_events_widgets.dart
@@ -13,27 +13,76 @@ class UpcomingEvents extends StatelessWidget {
   Widget build(BuildContext context) {
     final events = eventProvider.events;
     final currentTime = DateTime.now();
-    final upcomingEvents = events.where((event) => event.from.isAfter(currentTime)).toList();
+    final upcomingEvents = events.where((event) => event.from.isAfter(currentTime) || (event.from.isBefore(currentTime) && event.to.isAfter(currentTime))).toList();
 
-    final eventList = upcomingEvents.take(5).toList();
+    final eventList = upcomingEvents.take(6).toList();
 
     eventList.sort((a, b) {
-      // Compare the dates first
       final dateTimeComparison = a.from.compareTo(b.from);
       return dateTimeComparison;
     });
 
-    int itemCount = eventList.length; // Replace with your actual item count
+    int itemCount = eventList.length;
 
     return Visibility(
-      visible: itemCount > 0, // Show the ListView if itemCount is greater than 0
+      visible: itemCount > 0,
       child: ListView.builder(
         itemCount: itemCount,
         itemBuilder: (context, index) {
+
           final event = eventList[index];
           final fromformattedTime = DateFormat('HH:mm').format(event.from);
           final toformattedTime = DateFormat('HH:mm').format(event.to);
           final formattedDate = DateFormat('EEEE').format(event.from);
+
+          if (index == 0 && eventList[index].from.isBefore(currentTime) && eventList[index].to.isAfter(currentTime)) {
+            return ListTile(
+              leading: Container(
+                height: 70,
+                width: 50,
+                decoration: BoxDecoration(
+                  color: darkBlue,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    event.from.day.toString(),
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ),
+              title: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    event.title,
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+              subtitle: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(formattedDate),
+                  Row(
+                    children: [
+                      Text(fromformattedTime),
+                      const Text(' - '),
+                      Text(toformattedTime),
+                    ],
+                  ),
+                ],
+              ),
+            );
+          }
           return ListTile(
             leading: Container(
               height: 70,
@@ -58,7 +107,8 @@ class UpcomingEvents extends StatelessWidget {
             title: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text(event.title,
+                Text(
+                  event.title,
                   style: const TextStyle(
                     fontWeight: FontWeight.bold,
                   ),
@@ -79,7 +129,7 @@ class UpcomingEvents extends StatelessWidget {
               ],
             ),
           );
-        }
+        },
       ),
       replacement: Lottie.asset(
         'assets/animations/empty.json',
@@ -88,6 +138,5 @@ class UpcomingEvents extends StatelessWidget {
         repeat: false,
       ),
     );
-
   }
 }
