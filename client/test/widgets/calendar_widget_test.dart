b3dbf5f29012a9fc08cb17627be9773e69670c28
LesibaFrans <u21566382@tuks.co.za>
2023-06-27 02:00:25 -0700

    more tests

--- client/test/widgets/calendar_widget_test.dart
+++ client/test/widgets/calendar_widget_test.dart
@@ -7,5 +7,5 @@ import 'package:syncfusion_flutter_calendar/calendar.dart';
 import 'package:client/widgets/calendar_widget.dart';
 import 'package:client/widgets/tasks_widget.dart';
 void main() {
-  
+
 }
