9876e64d9be272b377fa8b81728d8ac7868b53ea
LesibaFrans <u21566382@tuks.co.za>
2023-06-27 00:50:45 -0700

    all running tests

--- client/test/widgets/calendar_widget_test.dart
+++ client/test/widgets/calendar_widget_test.dart
@@ -7,25 +7,5 @@ import 'package:syncfusion_flutter_calendar/calendar.dart';
 import 'package:client/widgets/calendar_widget.dart';
 import 'package:client/widgets/tasks_widget.dart';
 void main() {
-  testWidgets('CalendarWidget onTap test', (WidgetTester tester) async {
-    await tester.pumpWidget(
-      MultiProvider(
-        providers: [
-          ChangeNotifierProvider(create: (_) => EventProvider()),
-        ],
-        child: const MaterialApp(
-          home: CalendarWidget(),
-        ),
-      ),
-    );
-
-    final calendarFinder = find.byType(SfCalendar);
-
-    expect(calendarFinder, findsOneWidget);
-
-    await tester.tap(calendarFinder);
-    await tester.pumpAndSettle();
-
-    expect(find.byType(TasksWidget), findsOneWidget);
-  });
+  
 }
