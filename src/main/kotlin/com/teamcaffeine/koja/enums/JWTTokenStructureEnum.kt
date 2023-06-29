dbeb2143922a8d51949cc32521b40852038010ab
Marthinus Ettienne van Zyl <u19012366@tuks.co.za>
2023-06-29 22:45:48 +0200

    Updated code to follow coding conventions and standards

--- src/main/kotlin/com/teamcaffeine/koja/enums/JWTTokenStructureEnum.kt
+++ src/main/kotlin/com/teamcaffeine/koja/enums/JWTTokenStructureEnum.kt
@@ -2,5 +2,5 @@ package com.teamcaffeine.koja.enums
 
 enum class JWTTokenStructureEnum(val claimName: String) {
     USER_ACCOUNT_TOKENS("userAccountTokens"),
-    USER_ID("userID")
+    USER_ID("userID"),
 }
