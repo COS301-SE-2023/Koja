8ae75d2301ebf395d1202e38a041ed707afd200f
Elias Makhonjwa <u18034332@tuks.co.za>
2023-07-11 17:38:22 +0200

    added structure of addAnotherEmail method

--- src/main/kotlin/com/teamcaffeine/koja/controller/UserAccountController.kt
+++ src/main/kotlin/com/teamcaffeine/koja/controller/UserAccountController.kt
@@ -1,4 +1,29 @@
 package com.teamcaffeine.koja.controller
 
-class UserAccountController {
+import com.teamcaffeine.koja.service.GoogleCalendarAdapterService
+import jakarta.servlet.http.HttpServletRequest
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.servlet.view.RedirectView
+
+@RestController
+@RequestMapping("/api/v1/auth/addEmail")
+
+class UserAccountManagerController(private val googleCalendarAdapter: GoogleCalendarAdapterService) {
+
+    @GetMapping("/google")
+    fun authenticateWithGoogle(request: HttpServletRequest): RedirectView {
+        return googleCalendarAdapter.setupConnection(request, false)
+    }
+
+    @GetMapping("/google/callback")
+    fun handleGoogleOAuth2Callback(@RequestParam("code") authCode: String?): ResponseEntity<String> {
+        val jwt = googleCalendarAdapter.oauth2Callback(authCode, false)
+        return ResponseEntity.ok()
+            .header("Authorization", "Bearer $jwt")
+            .body("Authentication successful")
+    }
 }
--- src/main/kotlin/com/teamcaffeine/koja/service/UserAccountManagerService.kt
+++ src/main/kotlin/com/teamcaffeine/koja/service/UserAccountManagerService.kt
@@ -1,4 +1,7 @@
 package com.teamcaffeine.koja.service
 
+import org.springframework.stereotype.Service
+
+@Service
 class UserAccountManagerService {
 }
