edf126dbb6b068e85dd425a735bb12c022869374
LTF Setsiba <u21566382@tuks.co.za>
2023-07-12 21:05:48 +0200

    new test files

---
+++ client/test/models/event_wrapper_module_test.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:timezone/standalone.dart' as tz;
+import 'package:client/models/event_wrapper_module.dart';
+
+void main() {
+  test("Tests running", () {
+    expect(1, 1);
+  });
+}
