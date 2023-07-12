5939e2ec76585ebf20df0ec0befbdb9c2841531b
LTF Setsiba <u21566382@tuks.co.za>
2023-07-12 21:47:10 +0200

    Update structured_format_model_test.dart

--- client/test/models/structured_format_model_test.dart
+++ client/test/models/structured_format_model_test.dart
@@ -2,7 +2,28 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:client/models/structured_format_model.dart';
 
 void main() {
-  test("Tests running", () {
-    expect(1, 1);
+  group('StructuredFormatting', () {
+    test('fromJson should create an instance with the correct values', () {
+      final json = {
+        'main_text': 'Main Text',
+        'secondary_text': 'Secondary Text',
+      };
+
+      final formatting = StructuredFormatting.fromJson(json);
+
+      expect(formatting.mainText, 'Main Text');
+      expect(formatting.secondaryText, 'Secondary Text');
+    });
+
+    test('fromJson should create an instance with null values if JSON fields are missing', () {
+      final json ={
+        'main_text': null,
+        'secondary_text': null,
+      };
+      final formatting = StructuredFormatting.fromJson(json);
+
+      expect(formatting.mainText, isNull);
+      expect(formatting.secondaryText, isNull);
+    });
   });
 }
