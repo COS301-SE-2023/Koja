4ff36a3bc9bb0b349cecaa16dd04da99483a5a2c
LTF Setsiba <u21566382@tuks.co.za>
2023-07-12 22:15:26 +0200

    Update location_predict_widget_test.dart

---
+++ client/test/models/location_predict_widget_test.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:client/models/autocomplete_predict_model.dart';
+
+void main(){
+  test("Test Running", (){
+    expect(1, 1);
+  });
+}
