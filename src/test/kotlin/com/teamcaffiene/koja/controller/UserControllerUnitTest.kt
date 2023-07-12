c732e23640082814995eae6c7a68aa03db04d2f9
Marthinus Ettienne van Zyl - u19012366 <u19012366@tuks.co.za>
2023-07-13 00:50:05 +0200

    Update tests for the UserController

--- src/test/kotlin/com/teamcaffiene/koja/controller/UserControllerUnitTest.kt
+++ src/test/kotlin/com/teamcaffiene/koja/controller/UserControllerUnitTest.kt
@@ -4,9 +4,11 @@ import com.teamcaffeine.koja.constants.ResponseConstant
 import com.teamcaffeine.koja.controller.TokenManagerController.Companion.createToken
 import com.teamcaffeine.koja.controller.TokenRequest
 import com.teamcaffeine.koja.controller.UserController
+import com.teamcaffeine.koja.dto.JWTGoogleDTO
 import com.teamcaffeine.koja.entity.UserAccount
 import com.teamcaffeine.koja.enums.AuthProviderEnum
 import com.teamcaffeine.koja.repository.UserAccountRepository
+import io.github.cdimascio.dotenv.Dotenv
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
@@ -19,6 +21,7 @@ import org.springframework.http.ResponseEntity
 
 class UserControllerUnitTest {
     private lateinit var userController: UserController
+    private lateinit var dotenv: Dotenv
 
     @Mock
     private lateinit var userAccountRepository: UserAccountRepository
@@ -26,13 +29,39 @@ class UserControllerUnitTest {
     @BeforeEach
     fun setup() {
         MockitoAnnotations.openMocks(this)
+        importEnvironmentVariables()
         userController = UserController(userAccountRepository)
+    }
+
+    private fun importEnvironmentVariables() {
+        dotenv = Dotenv.load()
+
+        dotenv["KOJA_AWS_RDS_DATABASE_URL"]?.let { System.setProperty("KOJA_AWS_RDS_DATABASE_URL", it) }
+        dotenv["KOJA_AWS_RDS_DATABASE_ADMIN_USERNAME"]?.let {
+            System.setProperty(
+                "KOJA_AWS_RDS_DATABASE_ADMIN_USERNAME",
+                it,
+            )
+        }
+        dotenv["KOJA_AWS_RDS_DATABASE_ADMIN_PASSWORD"]?.let {
+            System.setProperty(
+                "KOJA_AWS_RDS_DATABASE_ADMIN_PASSWORD",
+                it,
+            )
+        }
+
+        dotenv["GOOGLE_CLIENT_ID"]?.let { System.setProperty("GOOGLE_CLIENT_ID", it) }
+        dotenv["GOOGLE_CLIENT_SECRET"]?.let { System.setProperty("GOOGLE_CLIENT_SECRET", it) }
+        dotenv["API_KEY"]?.let { System.setProperty("API_KEY", it) }
+
+        dotenv["KOJA_JWT_SECRET"]?.let { System.setProperty("KOJA_JWT_SECRET", it) }
     }
 
     @Test
     fun testDeleteUserAccount() {
-        val mockUserID = 1
-        val mockToken = createToken(TokenRequest(emptyList(), AuthProviderEnum.GOOGLE, 0))
+        val mockUserID = Int.MAX_VALUE
+        val authDetails = JWTGoogleDTO("access", "refresh", 60 * 60)
+        val mockToken = createToken(TokenRequest(arrayListOf(authDetails), AuthProviderEnum.GOOGLE, mockUserID))
 
         val userAccount = UserAccount(id = 1, email = "test@test.com", refreshToken = "refresh", authProvider = AuthProviderEnum.GOOGLE, userID = mockUserID)
         val userAccounts = listOf(userAccount)
@@ -42,7 +71,7 @@ class UserControllerUnitTest {
         val result = userController.deleteUserAccount(mockToken)
 
         assertEquals(result, ResponseEntity.ok(ResponseConstant.ACCOUNT_DELETED))
-        Mockito.verify(userAccountRepository, Mockito.times(1)).delete(userAccount)
+        Mockito.verify(userAccountRepository).delete(userAccount)
     }
 
     @Test
