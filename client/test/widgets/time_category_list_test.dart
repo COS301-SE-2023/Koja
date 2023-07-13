5455365af0ccae5f9ab8e682628e90d089d6f7da
LesibaFrans <u21566382@tuks.co.za>
2023-07-13 20:27:08 +0200

    Update time_category_list_test.dart

--- client/test/widgets/time_category_list_test.dart
+++ client/test/widgets/time_category_list_test.dart
@@ -1,8 +1,7 @@
 import 'package:client/widgets/time_category_list.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:icons_plus/icons_plus.dart';
-import 'package:flutter_slidable/flutter_slidable.dart';
+
 
 void main() {
   testWidgets('TimeCategory Widget Test', (WidgetTester tester) async {
@@ -50,5 +49,6 @@ void main() {
 
     // Verify that the edit button is not pressed initially
     expect(editPressed, false);
+
   });
 }
