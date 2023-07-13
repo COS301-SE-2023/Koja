058a263ee3ea68a02ef7f0f65cb24a12ca2f421d
LesibaFrans <u21566382@tuks.co.za>
2023-07-13 20:39:26 +0200

    Update calendar_widget_test.dart

--- client/test/widgets/calendar_widget_test.dart
+++ client/test/widgets/calendar_widget_test.dart
@@ -1,1 +1,27 @@
-void main() {}
+import 'package:client/widgets/tasks_widget.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:provider/provider.dart';
+import 'package:syncfusion_flutter_calendar/calendar.dart';
+
+import 'package:client/providers/event_provider.dart';
+import 'package:client/widgets/calendar_widget.dart';
+
+void main() {
+  testWidgets('CalendarWidget Test', (WidgetTester tester) async {
+    // Create a mock EventProvider
+    final eventProvider = EventProvider();
+
+    await tester.pumpWidget(
+      MaterialApp(
+        home: ChangeNotifierProvider<EventProvider>.value(
+          value: eventProvider,
+          child: CalendarWidget(),
+        ),
+      ),
+    );
+
+   
+  });
+}
+
