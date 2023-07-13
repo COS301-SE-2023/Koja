c0739e9c964651085782a12177fbffc590adc405
LesibaFrans <u21566382@tuks.co.za>
2023-07-13 19:19:53 +0200

    create tasks screen tests

---
+++ client/test/screens/tasks_screen_test.dart
@@ -0,0 +1,26 @@
+import 'package:provider/provider.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:client/screens/tasks_screen.dart';
+import 'package:client/providers/event_provider.dart';
+
+void main() {
+  testWidgets('Tasks Screen Test', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      MaterialApp(
+        home: MultiProvider(
+          providers: [
+            ChangeNotifierProvider<EventProvider>(
+              create: (_) => EventProvider(),
+            ),
+          ],
+          child: Tasks(),
+        ),
+      ),
+    );
+
+    // Verify the existence of the Tasks screen
+    expect(find.byType(Tasks), findsOneWidget);
+
+  });
+}
