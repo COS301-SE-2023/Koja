78266a90069dc9a417bf18272fee1ff7e4b1cc24
LTF Setsiba <u21566382@tuks.co.za>
2023-07-13 10:50:17 +0200

    Create service_provider_test.dart

---
+++ client/test/providers/service_provider_test.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:client/providers/service_provider.dart';
+
+void main(){
+  test("Tests running", () {
+    expect(true, true);
+  });
+}
