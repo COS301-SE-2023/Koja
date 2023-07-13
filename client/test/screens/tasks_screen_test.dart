c32dd85fdfc417bee3ec6a09360aca6ced1bfe05
LesibaFrans <u21566382@tuks.co.za>
2023-07-13 19:27:12 +0200

    Update tasks_screen_test.dart

--- client/test/screens/tasks_screen_test.dart
+++ client/test/screens/tasks_screen_test.dart
@@ -1,3 +1,4 @@
+import 'package:client/screens/suggestions_screens.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -33,6 +34,16 @@ void main() {
 
     // Verify the existence of the CurrentTasksScreen
     expect(find.byType(CurrentTasksScreen), findsOneWidget);
+
+    // Verify the SuggestionsTasksScreen is not present before button is pressed
+    expect(find.byType(SuggestionsTasksScreen), findsNothing);
+
+    // Tap on the Suggestions tab
+    await tester.tap(find.text('Suggestions'));
+    await tester.pumpAndSettle();
+
+    // Verify that the SuggestionTaskScreen is found after the button is pressed
+    expect(find.byType(SuggestionsTasksScreen), findsOneWidget);
 
   });
 }
