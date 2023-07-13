e4b5dab6ce7cd3d562e6df987f7f17af5eee87ca
Marthinus Ettienne van Zyl - u19012366 <u19012366@tuks.co.za>
2023-07-13 12:12:25 +0200

    Fix server setup & get user emails improvements

--- client/lib/providers/service_provider.dart
+++ client/lib/providers/service_provider.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 
 import 'package:client/providers/event_provider.dart';
 import 'package:flutter/foundation.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_web_auth/flutter_web_auth.dart';
 import 'package:http/http.dart' as http;
 import 'package:fl_location/fl_location.dart';
@@ -10,6 +11,9 @@ import 'package:fl_location/fl_location.dart';
 import '../Utils/event_util.dart';
 
 class ServiceProvider with ChangeNotifier {
+  late String _serverAddress;
+  late String _serverPort;
+
   String? _accessToken;
   Location? _locationData;
   StreamSubscription<Location>? _locationSubscription;
@@ -27,6 +31,8 @@ class ServiceProvider with ChangeNotifier {
 
   Future<ServiceProvider> init() async {
     startLocationListner();
+    _serverAddress = dotenv.get("SERVER_ADDRESS", fallback: "localhost");
+    _serverPort = dotenv.get("SERVER_PORT", fallback: "8080");
     return this;
   }
 
@@ -42,7 +48,8 @@ class ServiceProvider with ChangeNotifier {
   }
 
   Future<bool> addEmail({required EventProvider eventProvider}) async {
-    final String authUrl = 'http://192.168.254.68:8080/app/v1/auth/addEmail/google';
+    final String authUrl =
+        'http://$_serverAddress:$_serverPort/app/v1/auth/addEmail/google';
 
     final String callbackUrlScheme = 'koja-login-callback';
 
@@ -58,23 +65,24 @@ class ServiceProvider with ChangeNotifier {
     return accessToken != null;
   }
 
-  Future<List<Event>> getAllUserEmails() async {
-    final url = Uri.http('192.168.254.68:8080', '/api/v1/user/linked-emails');
+  Future<List<String>> getAllUserEmails() async {
+    final url =
+        Uri.http('$_serverAddress:$_serverPort', '/api/v1/user/linked-emails');
     final response = await http.get(
       url,
       headers: {'Authorisation': _accessToken!},
     );
 
     if (response.statusCode == 200) {
-      final eventsJson = jsonDecode(response.body);
-      return eventsJson.map((json) => Event.fromJson(json)).toList();
+      return List.of(jsonDecode(response.body));
     } else {
       return [];
     }
   }
 
   Future<bool> loginUser({required EventProvider eventProvider}) async {
-    final String authUrl = 'http://192.168.254.68:8080/api/v1/auth/app/google';
+    final String authUrl =
+        'http://$_serverAddress:$_serverPort/api/v1/auth/app/google';
 
     final String callbackUrlScheme = 'koja-login-callback';
 
@@ -91,7 +99,8 @@ class ServiceProvider with ChangeNotifier {
   }
 
   Future<bool> createEvent(Event event) async {
-    final url = Uri.http('192.168.254.68:8080', '/api/v1/user/calendar/createEvent');
+    final url = Uri.http(
+        '$_serverAddress:$_serverPort', '/api/v1/user/calendar/createEvent');
     final response = await http.post(
       url,
       headers: {
@@ -105,7 +114,8 @@ class ServiceProvider with ChangeNotifier {
   }
 
   Future<List<Event>> getAllUserEvents() async {
-    final url = Uri.http('192.168.254.68:8080', '/api/v1/user/calendar/userEvents');
+    final url = Uri.http(
+        '$_serverAddress:$_serverPort', '/api/v1/user/calendar/userEvents');
     final response = await http.get(
       url,
       headers: {'Authorisation': _accessToken!},
@@ -120,7 +130,8 @@ class ServiceProvider with ChangeNotifier {
   }
 
   Future<bool> updateEvent(Event event) async {
-    final url = Uri.http('192.168.254.68:8080', '/api/v1/user/calendar/updateEvent');
+    final url = Uri.http(
+        '$_serverAddress:$_serverPort', '/api/v1/user/calendar/updateEvent');
     final response = await http.put(
       url,
       headers: {
@@ -134,7 +145,8 @@ class ServiceProvider with ChangeNotifier {
   }
 
   Future<bool> deleteEvent(String eventId) async {
-    final url = Uri.http('192.168.254.68:8080', '/api/v1/user/calendar/deleteEvent');
+    final url = Uri.http(
+        '$_serverAddress:$_serverPort', '/api/v1/user/calendar/deleteEvent');
     final response = await http.delete(
       url,
       headers: {
@@ -153,7 +165,8 @@ class ServiceProvider with ChangeNotifier {
 
   Future<int> getLocationsTravelTime(
       String placeID, double destLat, double destLng) async {
-    final url = Uri.http('192.168.254.68:8080', '/api/v1/location/travel-time', {
+    final url = Uri.http(
+        '$_serverAddress:$_serverPort', '/api/v1/location/travel-time', {
       'placeId': placeID,
       'destLat': destLat.toString(),
       'destLng': destLng.toString(),
