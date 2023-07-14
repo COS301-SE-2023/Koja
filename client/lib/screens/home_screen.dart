dc45e92200e3fcead7312d1394dd7efb799cafed
UnarineLeo <unarineleo@gmail.com>
2023-07-14 14:11:58 +0200

    update display of location and time

--- client/lib/screens/home_screen.dart
+++ client/lib/screens/home_screen.dart
@@ -177,7 +177,7 @@ class HomeState extends State<Home> {
                     showDialog(
                       context: context,
                       builder: (BuildContext context) {
-                        return LocationListWidget(locationNames: [],);
+                        return LocationListWidget();
                       },
                     );
                   },
--- client/lib/widgets/location_list_widget.dart
+++ client/lib/widgets/location_list_widget.dart
@@ -1,13 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
 
-class LocationListWidget extends StatelessWidget {
-  final List<String> locationNames;
-
+class LocationListWidget extends StatefulWidget {
   const LocationListWidget({
     Key? key,
-    required this.locationNames,
   }) : super(key: key);
+
+  @override
+  State<LocationListWidget> createState() => _LocationListWidgetState();
+}
+
+class _LocationListWidgetState extends State<LocationListWidget> {
+  List<List<String>> locationList = [
+    ['Apartments On Williams Residence', '50min'],
+    ['School', '1h 30min'],
+    ['Work', '1h 30min'],
+    ['Hobbies', '1h 30min'],
+    ['Resting', '1h 30min'],
+    ['Chores', '1h 30min'],
+  ];
+
+  void delete(int index) {
+    setState(() {
+      locationList.removeAt(index);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,32 +33,31 @@ class LocationListWidget extends StatelessWidget {
       title: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          if(locationNames.isEmpty)
-          Center(
-            child: Text(
-              'No Saved Locations',
-              style: TextStyle(
-                fontSize: 17,
-                fontWeight: FontWeight.bold,
+          if (locationList.isEmpty)
+            Center(
+              child: Text(
+                'No Saved Locations',
+                style: TextStyle(
+                  fontSize: 17,
+                  fontWeight: FontWeight.bold,
+                ),
+                maxLines: 1,
+                textAlign: TextAlign.center,
               ),
-              maxLines: 1,
-              textAlign: TextAlign.center,
             ),
-          ),
-
-          if(locationNames.isNotEmpty)
-          Center(
-            child: Text(
-              'From Current Location',
-              style: TextStyle(
-                fontSize: 17,
-                fontWeight: FontWeight.bold,
-                decoration: TextDecoration.underline,
+          if (locationList.isNotEmpty)
+            Center(
+              child: Text(
+                'From Current Location',
+                style: TextStyle(
+                  fontSize: 17,
+                  fontWeight: FontWeight.bold,
+                  decoration: TextDecoration.underline,
+                ),
+                maxLines: 1,
+                textAlign: TextAlign.center,
               ),
-              maxLines: 1,
-              textAlign: TextAlign.center,
             ),
-          ),
           GestureDetector(
             onTap: () {
               Navigator.of(context).pop();
@@ -55,53 +71,75 @@ class LocationListWidget extends StatelessWidget {
       ),
       backgroundColor: Colors.grey[200],
       contentPadding: EdgeInsets.all(16),
-      content: 
-      Column(
+      content: Column(
         children: [
-          if(locationNames.isEmpty)
-          Lottie.asset(
-            'assets/animations/empty.json',
-            height: 150,
-            width: 300,
-            repeat: false,
-          ),
-
-          if(locationNames.isNotEmpty)
-          Align(
-            alignment: Alignment.centerLeft,
-            child: Text(
-              "To:",
-              style: TextStyle(
-                fontSize: 15.5,
-                fontWeight: FontWeight.bold,
-                decorationStyle: TextDecorationStyle.wavy,
+          if (locationList.isEmpty)
+            Lottie.asset(
+              'assets/animations/empty.json',
+              height: 150,
+              width: 300,
+              repeat: false,
+            ),
+          if (locationList.isNotEmpty)
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                "To:",
+                style: TextStyle(
+                  fontSize: 15.5,
+                  fontWeight: FontWeight.bold,
+                  decorationStyle: TextDecorationStyle.wavy,
+                ),
               ),
             ),
-          ),
-          
-          if(locationNames.isNotEmpty)
-          Container(
-            height: MediaQuery.of(context).size.height * 0.8,
-            width: MediaQuery.of(context).size.width * 0.8,
-            child: ListView.builder(
-              itemCount:5,
-              itemBuilder: (context, index) {
-                final locationName = locationNames[index];
-                return ListTile(
-                  title: Text(locationName),
-                  trailing: GestureDetector(
-                    onTap: () {
-                      Navigator.of(context).pop();
-                    },
-                    child: Icon(Icons.delete_outline),
-                  ),
-                  // onTap: () {
-                  //   Navigator.of(context).pop();
-                  // },
-                );
-              },
+          if (locationList.isNotEmpty)
+            Container(
+              height: MediaQuery.of(context).size.height * 0.8,
+              width: MediaQuery.of(context).size.width * 0.8,
+              child: ListView.builder(
+                itemCount: locationList.length,
+                itemBuilder: (context, index) {
+                  final locationName = locationList[index][0];
+                  final locationTime = locationList[index][1];
+                  return ListTile(
+                    title: Row(
+                      children: [
+                        Expanded(
+                          child: Text(
+                            "$locationName",
+                            style: TextStyle(
+                              fontSize: 15,
+                              fontWeight: FontWeight.bold,
+                            ),
+                            maxLines: 1,
+                          ),
+                        ),
+                        Text(
+                          " - ",
+                          style: TextStyle(
+                            fontSize: 15,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          locationTime,
+                          style: TextStyle(
+                            fontSize: 15,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                    trailing: GestureDetector(
+                      onTap: () {
+                        delete(index);
+                      },
+                      child: Icon(Icons.delete_outline),
+                    ),
+                  );
+                },
+              ),
             ),
-          ),
         ],
       ),
     );
