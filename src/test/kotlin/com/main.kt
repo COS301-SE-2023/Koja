995e2e46ce9f9ee0d276899f20b81818f1a13dab
Marthinus Ettienne van Zyl - u19012366 <u19012366@tuks.co.za>
2023-07-21 21:33:35 +0200

    Commented unused code

--- src/test/kotlin/com/main.kt
+++ src/test/kotlin/com/main.kt
@@ -1,16 +1,16 @@
-package com
-
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
-
-
-    @RunWith(Suite.class)
-        @Suite.SuiteClasses({
-            TestClass1.class,
-            TestClass2.class,
-            TestClass3.class
-            // Add more test classes as needed
-        })
-        public class main {
-            // This class can be empty
-        }
+//package com
+//
+//import org.junit.runner.RunWith;
+//import org.junit.runners.Suite;
+//
+//
+//    @RunWith(Suite.class)
+//        @Suite.SuiteClasses({
+//            TestClass1.class,
+//            TestClass2.class,
+//            TestClass3.class
+//            // Add more test classes as needed
+//        })
+//        public class main {
+//            // This class can be empty
+//        }
