439a6ec02d88a06e842af0671d345b788982ba97
Marthinus Ettienne van Zyl - u19012366 <u19012366@tuks.co.za>
2023-07-25 08:55:15 +0200

    Add timeframes as a feature

--- AI/main.py
+++ AI/main.py
@@ -60,24 +60,30 @@ for block in data:
     # Concatenate training and testing data
     all_events = expanded_training_events + expanded_testing_events
 
-print(all_events)
 all_df = pd.DataFrame(all_events)
 
 # Extract user_ids and category_ids as numpy arrays
 user_ids = all_df['userID'].values
 category_ids = all_df['category'].values
 weekdays = all_df['weekday'].values
+time_frames = []
+
+for timeFrame in all_df[['startTime', 'endTime']].values:
+    time_frames.append(f"{timeFrame[0]}-{timeFrame[1]}")
 
 # Create TensorFlow dataset from pandas DataFrame
 all_data = tf.data.Dataset.from_tensor_slices({
     "user_id": user_ids,
     "category_id": category_ids,
-    "weekday": weekdays
+    "weekday": weekdays,
+    "time_frames": time_frames
 })
-
-# We will also need a dataset of all unique user ids and category ids for model fitting
+#
+# # We will also need a dataset of all unique user ids and category ids for model fitting
 user_dataset = tf.data.Dataset.from_tensor_slices(np.unique(user_ids))
 category_dataset = tf.data.Dataset.from_tensor_slices(np.unique(category_ids))
+weekday_dataset = tf.data.Dataset.from_tensor_slices(np.unique(weekdays))
+time_frame_dataset = tf.data.Dataset.from_tensor_slices(np.unique(time_frames))
 
 user_ids_vocabulary = tf.keras.layers.StringLookup(mask_token=None)
 user_ids_vocabulary.adapt(user_dataset)
@@ -85,55 +91,69 @@ user_ids_vocabulary.adapt(user_dataset)
 categories_vocabulary = tf.keras.layers.StringLookup(mask_token=None)
 categories_vocabulary.adapt(category_dataset)
 
-# Define user and category models
+weekday_vocabulary = tf.keras.layers.StringLookup(mask_token=None)
+weekday_vocabulary.adapt(weekday_dataset)
+
+time_frame_vocabulary = tf.keras.layers.StringLookup(mask_token=None)
+time_frame_vocabulary.adapt(time_frame_dataset)
+
+# # Define user and category models
 user_model = tf.keras.Sequential([
     user_ids_vocabulary,
-    tf.keras.layers.Embedding(len(np.unique(user_ids)) + 1, 16),
+    tf.keras.layers.Embedding(user_ids_vocabulary.vocab_size() + 1, 16),
 ])
 
 category_model = tf.keras.Sequential([
     categories_vocabulary,
-    tf.keras.layers.Embedding(len(np.unique(category_ids)) + 1, 16),
+    tf.keras.layers.Embedding(categories_vocabulary.vocab_size() + 1, 16),
 ])
 
 weekday_model = tf.keras.Sequential([
-    tf.keras.layers.StringLookup(vocabulary=np.unique(weekdays), mask_token=None),
-    tf.keras.layers.Embedding(len(np.unique(weekdays)) + 1, 16),
+    weekday_vocabulary,
+    tf.keras.layers.Embedding(weekday_vocabulary.vocab_size() + 1, 16),
 ])
 
-# Define the task as retrieval (recommendation)
+time_frame_model = tf.keras.Sequential([
+    time_frame_vocabulary,
+    tf.keras.layers.Embedding(time_frame_vocabulary.vocab_size() + 1, 16),
+])
+#
+# # Define the task as retrieval (recommendation)
 task = tfrs.tasks.Retrieval(metrics=tfrs.metrics.FactorizedTopK(
     candidates=category_dataset.batch(128).map(category_model)
 ))
 
-# Create a TFRS model
+# # Create a TFRS model
 class CategoryRecommender(tfrs.models.Model):
-    def __init__(self, user_model, category_model, weekday_model, task):
+    def __init__(self, user_model, category_model, weekday_model, time_frame_model, task):
         super().__init__()
         self.user_model = user_model
         self.category_model = category_model
         self.weekday_model = weekday_model
+        self.time_frame_model = time_frame_model
         self.task = task
 
     def compute_loss(self, features, training=False):
         user_embeddings = self.user_model(features["user_id"])
         category_embeddings = self.category_model(features["category_id"])
         weekday_embeddings = self.weekday_model(features["weekday"])
+        time_frame_embeddings = self.time_frame_model(features["time_frames"])
 
         # Compute losses for each type of embeddings
         user_loss = self.task(user_embeddings, category_embeddings)
         category_loss = self.task(category_embeddings, category_embeddings)
         weekday_loss = self.task(weekday_embeddings, category_embeddings)
+        time_frame_loss = self.task(time_frame_embeddings, category_embeddings)
 
         # Combine these losses
-        return user_loss + category_loss + weekday_loss
+        return user_loss + category_loss + weekday_loss + time_frame_loss
 
-model = CategoryRecommender(user_model, category_model, weekday_model, task)
+model = CategoryRecommender(user_model, category_model, weekday_model, time_frame_model, task)
 
-# Configure and train the model
+# # Configure and train the model
 model.compile(optimizer=tf.keras.optimizers.Adagrad(learning_rate=0.008))
 
-# Train the model
+# # Train the model
 model.fit(all_data.batch(128), epochs=5)
 
 index = tfrs.layers.factorized_top_k.BruteForce(model.user_model)
@@ -141,9 +161,8 @@ index.index_from_dataset(
     tf.data.Dataset.zip((category_dataset.batch(100), category_dataset.batch(100).map(model.category_model)))
 )
 
-# Get recommendations.
+# # Get recommendations.
 user_id = "502"  # change this to the user id you want to recommend for
 _, titles = index(np.array([user_id]))
-
 
 print(f"Recommendations for user {user_id}: {titles[0][:10]}")
