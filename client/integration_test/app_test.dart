9c11e5b2d972d83f62562a263c2d5b745284656d
LesibaFrans <u21566382@tuks.co.za>
2023-07-26 14:31:14 +0200

    Integration test working codev1

--- client/integration_test/app_test.dart
+++ client/integration_test/app_test.dart
@@ -1,1 +1,24 @@
-void main() {}
+import 'package:client/main.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
+import 'package:client/main.dart' as app;
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+void main() {
+  setUp(() async{
+    await dotenv.load(fileName: "assets/.env");
+  });
+
+  group('App Test', () {
+    IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+    testWidgets('TimeBoundaries widget test', (WidgetTester tester) async {
+      await tester.pumpWidget(KojaApp());
+      await tester.pumpAndSettle();
+      expect(find.byType(SplashScreen), findsOneWidget);
+      await Future.delayed(Duration(seconds: 3));
+
+    });
+  });
+
+  //TODO: Add integration tests here
+}
+
