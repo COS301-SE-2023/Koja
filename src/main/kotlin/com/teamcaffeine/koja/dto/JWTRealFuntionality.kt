8dbefad652d1c94413318f096daf855b708b2e85
MatjereJ <matjere274@gmail.com>
2023-07-26 10:46:06 +0200

    adding location unit tests

---
+++ src/main/kotlin/com/teamcaffeine/koja/dto/JWTRealFuntionality.kt
@@ -0,0 +1,66 @@
+package com.teamcaffeine.koja.dto
+
+import com.auth0.jwt.JWT
+import com.auth0.jwt.algorithms.Algorithm
+import com.google.gson.Gson
+import com.teamcaffeine.koja.controller.TokenManagerController
+import com.teamcaffeine.koja.enums.AuthProviderEnum
+import org.springframework.stereotype.Service
+import java.security.MessageDigest
+import java.util.Base64
+import javax.crypto.Cipher
+import javax.crypto.SecretKey
+import javax.crypto.spec.SecretKeySpec
+
+@Service
+class JWTRealFuntionality : JWTFunctionality {
+    override fun createJWTToken(accessTokens: List<JWTAuthDetailsDTO>, expiryTime: Long, userID: Int): String {
+        return TokenManagerController.createJwtToken(accessTokens, expiryTime, userID)
+    }
+
+    override fun getUserJWTTokenData(jwtToken: String): UserJWTTokenDataDTO {
+        val jwtSecret: String = System.getProperty("KOJA_JWT_SECRET")
+        val algorithm = Algorithm.HMAC512(jwtSecret)
+        val verifier = JWT.require(algorithm).build()
+        val decodedJWT = verifier.verify(jwtToken)
+
+        val encryptedClaims = decodedJWT.getClaim("encrypted").asString()
+        val decryptedClaims =
+            decrypt(encryptedClaims, generateSecretKey(jwtSecret))
+        val gson = Gson()
+
+        val userTokens = gson.fromJson(
+            decryptedClaims.substringAfter("userAccountTokens=")
+                .substringBefore(", userID"),
+            List::class.java,
+        )
+
+        val userAccountTokens = mutableListOf<JWTAuthDetailsDTO>()
+
+        for (token in userTokens) {
+            val currentToken = JWTAuthDetailsDTO.parseJWTFormatString(token.toString())
+            if (currentToken?.authProvider == AuthProviderEnum.GOOGLE) {
+                userAccountTokens.add(currentToken as JWTGoogleDTO)
+            }
+        }
+
+        return UserJWTTokenDataDTO(
+            userAccountTokens,
+            decryptedClaims.substringAfter("userID=").substringBefore("}").toInt(),
+        )
+    }
+
+    override fun decrypt(text: String, key: SecretKey): String {
+        val cipher = Cipher.getInstance("AES")
+        cipher.init(Cipher.DECRYPT_MODE, key)
+        val decoded = Base64.getDecoder().decode(text)
+        val decrypted = cipher.doFinal(decoded)
+        return String(decrypted, Charsets.UTF_8)
+    }
+
+    override fun generateSecretKey(jwtSecret: String): SecretKey {
+        val messageDigest = MessageDigest.getInstance("SHA-256")
+        val keyBytes = messageDigest.digest(jwtSecret.toByteArray(Charsets.UTF_8))
+        return SecretKeySpec(keyBytes, "AES")
+    }
+}
--- src/test/kotlin/com/teamcaffiene/koja/service/LocationServiceTest.kt
+++ src/test/kotlin/com/teamcaffiene/koja/service/LocationServiceTest.kt
@@ -1,31 +1,57 @@
 package com.teamcaffiene.koja.service
 
-import com.teamcaffeine.koja.repository.UserAccountRepository
+import com.teamcaffeine.koja.KojaApplication
+import com.teamcaffeine.koja.dto.JWTAuthDetailsDTO
+import com.teamcaffeine.koja.dto.JWTFunctionality
+import com.teamcaffeine.koja.dto.UserJWTTokenDataDTO
+import com.teamcaffeine.koja.entity.User
 import com.teamcaffeine.koja.repository.UserRepository
 import com.teamcaffeine.koja.service.LocationService
+import com.teamcaffeine.koja.service.UserCalendarService
 import io.github.cdimascio.dotenv.Dotenv
-import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.mockito.Mock
+import org.mockito.Mockito
 import org.mockito.MockitoAnnotations
+import org.mockito.invocation.InvocationOnMock
+import org.mockito.kotlin.any
+import org.mockito.kotlin.check
 import org.mockito.kotlin.spy
+import org.mockito.kotlin.whenever
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig
+import java.util.Optional
 
+@SpringJUnitConfig
+@SpringBootTest(classes = [KojaApplication::class], webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
 class LocationServiceTest {
 
     @Mock
     lateinit var userRepository: UserRepository
 
     @Mock
-    lateinit var userAccountRepository: UserAccountRepository
+    private lateinit var jwtFunctionality: JWTFunctionality
 
-    private lateinit var service: LocationService
+    @Mock
+    private lateinit var locationService: LocationService
     private lateinit var dotenv: Dotenv
 
     @BeforeEach
     fun setup() {
         MockitoAnnotations.openMocks(this)
 
+        importEnvironmentVariables()
+
+        locationService = spy(LocationService())
+    }
+
+    private fun importEnvironmentVariables() {
         dotenv = Dotenv.load()
 
         dotenv["KOJA_AWS_RDS_DATABASE_URL"]?.let { System.setProperty("KOJA_AWS_RDS_DATABASE_URL", it) }
@@ -47,12 +73,85 @@ class LocationServiceTest {
         dotenv["API_KEY"]?.let { System.setProperty("API_KEY", it) }
 
         dotenv["KOJA_JWT_SECRET"]?.let { System.setProperty("KOJA_JWT_SECRET", it) }
-
-        service = spy(LocationService())
     }
+    /*  @BeforeEach
+    fun setup() {
+        val dotenv: Dotenv = Dotenv.load()
+        System.setProperty("KOJA_JWT_SECRET", dotenv["KOJA_JWT_SECRET"]!!)
+        userRepository = mockk()
+        userAccountRepository = mockk()
+        userCalendarService = UserCalendarService(userRepository)
+    }*/
 
+//    @Test
+//    fun getAllUserEvents_ReturnsEmptyList_WhenNoUserAccountsFound() {
+//        // Arrange
+//        val token = "***redacted***"
+//        val userJWTTokenDataDTO = UserJWTTokenDataDTO(userID = 123, userAuthDetails = emptyList())
+//        every { getUserJWTTokenData(token) } returns userJWTTokenDataDTO
+//        every { userAccountRepository.findByUserID(userJWTTokenDataDTO.userID) } returns emptyList()
+//
+//        // Act
+//        val result = userCalendarService.getAllUserEvents(token)
+//
+//        // Assert
+//        assertEquals(emptyList<UserEventDTO>(), result)
+//    }
+
+    //    @Test
+//    fun getAllUserEvents_ReturnsUserEventsFromCalendarAdapters() {
+//        val token = "***redacted***"
+//        val userJWTTokenDataDTO = UserJWTTokenDataDTO(
+//            userID = 123,
+//            userAuthDetails = listOf(JWTGoogleDTO(token, "dummyRefresh", 1L))
+//        )
+//        val userAccount = listOf(UserAccount(123))
+//        val calendarAdapterFactoryService = mockk<CalendarAdapterFactoryService>()
+//        val calendarAdapterService = mockk<CalendarAdapterService>()
+//        val userEvents = arrayListOf<UserEventDTO>()
+//
+//        every { getUserJWTTokenData(token) } returns userJWTTokenDataDTO
+//        every { userAccountRepository.findByUserID(userJWTTokenDataDTO.userID) } returns userAccount
+//        every { userCalendarService.getAllUserEvents(token) } returns userEvents
+//        every { calendarAdapterFactoryService.createCalendarAdapter(any()) } returns calendarAdapterService
+//
+//        val result = userCalendarService.getAllUserEvents(token)
+//
+//        assertEquals(userEvents, result)
+//    }
     @Test
-    fun addition() {
-        assertEquals(2, 1 + 1)
+    fun `addTimeBoundary should return true when the timeBoundary is valid and user exists`() {
+        // Arrange
+        val mockUserID = Int.MAX_VALUE
+        val userAccounts = mutableListOf<JWTAuthDetailsDTO>()
+        val mockUserJWTData = UserJWTTokenDataDTO(userAccounts, mockUserID)
+        val jwtToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJlbmNyeXB0ZWQiOiIrT09EZXlqWk1CWjV6b3I1OFRBdVZIT2x3akJvbHY1MGFkTjQ5ajdFMWMweW9TWW9zOTNHMUVWT0ord3Z2dGFLQ1RBbE43TzB1Z3crZm1JUmxGZ25jb3EyUHcxRHc2S2RQMVcydnRFWGdIMlFOTFRLSk1sTGkwOGsrWmtsbzRVM1pTcllJb3hzaFVvSk5YU1lnMmQ2K2tRWUpNUEt3MS9ndi9JWjA4U3BHWnpNM3lJaWFlQUt0UFgwZGVQWUlOSk1ZMm1iTlhnZVVIei9Bb3NtWUthNEY3MmlkSEgra0M4N2x6T1ROcU1mSnVCNHRadE40OVJCVlVxQ0JOQ3VrQzNjOUd6NHB3SHVkVVpnbjVnaXFPaXNDNCtSUXo3Uk9EN2I4R2YzdHpJMDZhR2ZURnZld05QSTFPT3JKb3JELzd2bDVHNEtScU1rZDFJaUlKNmd3SUFZanh5bHh2ck0vUHIvUngyWTdOcmRneTBHNHhXMGZjenkzUDhoQmhsYkNSOGFMVEdZdXF0dVdNOUNTMWJmY2hMS2E4UzBvR1pycVR0ak42QzdBOENEZVNHN29VR1ZKOUViSlBLaDVQMlpxVEpKWFlOYWVUclZEQkMxQkxPQ01JeE9QdU1DTWdreHczNWp2bFkyMmFDcUdFZHRJTy9UOXJIMjl4RWJHQUlXeUQvcjNTaW9KT1d2ZzJvd3B5NExKSitTZWVaVjU4UEtCL2wrVFZzTmZBRzB3Vlk2azMxNFk5R2xTbTROUVcyNFRyeDhja01FdHg1eFE1RHhBS0NOWnBXN2pnPT0iLCJleHAiOjE2ODk3MTY5MDR9.uctphVFxJICf8OexH0ZQHWONI3rTExoyDvdAlMdxMGUQaLjGmONyyt6sGP2wn2DUUtW9M1Mg-kbelZpU-zPgbQ"
+
+        val timeBoundary = TimeBoundary("Partying", "12:00", "05:00")
+        val mockUser = User()
+        val optionalUserValue = Optional.of(mockUser)
+
+        whenever(jwtFunctionality.getUserJWTTokenData(jwtToken)).thenReturn(mockUserJWTData)
+        whenever(userRepository.findById(mockUserID)).thenReturn(optionalUserValue)
+        whenever(userRepository.save(any<User>())).thenAnswer { invocation: InvocationOnMock -> invocation.getArgument<User>(0) }
+
+        // Act
+        val result = userCalendarService.addTimeBoundary(jwtToken, timeBoundary)
+
+        // Assert
+        Assertions.assertTrue(result)
+
+        // Verify that the time boundary is added to the user and saved in the repository
+        Assertions.assertEquals(timeBoundary, mockUser.getUserTimeBoundaries()[0])
+
+        // Verify that the user is saved in the repository
+        Mockito.verify(userRepository).save(
+            check<User> { savedUser ->
+                Assertions.assertEquals(mockUser, savedUser)
+                Assertions.assertEquals(timeBoundary, savedUser.getUserTimeBoundaries()[0])
+            },
+        )
     }
+
+
 }
