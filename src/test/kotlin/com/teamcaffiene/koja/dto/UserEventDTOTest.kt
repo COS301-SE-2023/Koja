82ff98a8e7ec34f28ac7e235d721894e6d1922a8
U18034332 <u18034332@tuks.co.za>
2023-07-26 16:49:15 +0200

    Added userDTOTest

--- src/test/kotlin/com/teamcaffiene/koja/dto/UserEventDTOTest.kt
+++ src/test/kotlin/com/teamcaffiene/koja/dto/UserEventDTOTest.kt
@@ -1,48 +1,52 @@
 package com.teamcaffiene.koja.dto
 
+import com.google.api.client.util.DateTime
+import com.google.api.services.calendar.model.Event
+import com.google.api.services.calendar.model.EventDateTime
+import com.teamcaffeine.koja.dto.UserEventDTO
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import java.time.OffsetDateTime
+
 class UserEventDTOTest {
 
-//    @Test
-//    fun createGoogleEventDTO_SUCCESS() {
-//        // Given
-//        val event = Event()
-//            .setId("1")
-//            .setDescription("A chance to hear more about Google's developer products.")
-//            .setLocation("800 Howard St., San Francisco, CA 94103")
-//        val startDateTime = DateTime("2015-05-28T09:00:00-07:00")
-//        val start = EventDateTime()
-//            .setDateTime(startDateTime)
-//            .setTimeZone("America/Los_Angeles")
-//        event.setStart(start)
-//
-//        val endDateTime = DateTime("2015-05-28T17:00:00-07:00")
-//        val end = EventDateTime()
-//            .setDateTime(endDateTime)
-//            .setTimeZone("America/Los_Angeles")
-//        event.setEnd(end)
-//
-//        // When
-//        val userEventDTO = UserEventDTO(event)
-//
-//        // Then
-//        assertEquals("A chance to hear more about Google's developer products.", userEventDTO.getDescription())
-//        assertEquals("1", userEventDTO.getId())
-//        assertEquals("800 Howard St., San Francisco, CA 94103", userEventDTO.getLocation())
-//        assertEquals(Date(DateTime("2015-05-28T09:00:00-07:00").value), userEventDTO.getStartTime())
-//    }
+    @Test
+    fun createGoogleEventDTO_SUCCESS() {
+        // Given
+        val event = Event()
+            .setId("1")
+            .setSummary("desc1")
+            .setLocation("800 Howard St., San Francisco, CA 94103")
+        val startDateTime = DateTime("2015-05-28T09:00:00-07:00")
+        val start = EventDateTime()
+            .setDateTime(startDateTime)
+            .setTimeZone("America/Los_Angeles")
+        event.setStart(start)
 
-//    @Test
-//
-//    fun `CreateUserEventDTO_SUCESS`() {
-//        // When
-//        val userEventDTO = UserEventDTO(
-//            "1", "5KM Morning Jog", "LC sports center", Date(2015, 5, 28, 7, 0), Date(2015, 5, 28, 9, 0)
-//        )
-//
-//        // Then
-//        assertEquals("1", userEventDTO.getId())
-//        assertEquals("5KM Morning Jog", userEventDTO.getDescription())
-//        assertEquals("LC sports center", userEventDTO.getLocation())
-//        assertEquals(Date(2015, 5, 28, 7, 0), userEventDTO.getStartTime())
-//    }
+        val endDateTime = DateTime("2015-05-28T17:00:00-07:00")
+        val end = EventDateTime()
+            .setDateTime(endDateTime)
+            .setTimeZone("America/Los_Angeles")
+        event.setEnd(end)
+
+        // When
+        val userEventDTO = UserEventDTO(event)
+
+        // Then
+        assertEquals("desc1", userEventDTO.getDescription())
+        assertEquals("1", userEventDTO.getId())
+        assertEquals("800 Howard St., San Francisco, CA 94103", userEventDTO.getLocation())
+    }
+
+    @Test
+
+    fun CreateUserEventDTO_SUCESS() {
+        // When
+        val event = UserEventDTO("1", "desc1", "loc1", OffsetDateTime.now().minusDays(2), OffsetDateTime.now().minusDays(1), 1, emptyList(), 1, false)
+
+        // Then
+        assertEquals("1", event.getId())
+        assertEquals("desc1", event.getDescription())
+        assertEquals("loc1", event.getLocation())
+    }
 }
