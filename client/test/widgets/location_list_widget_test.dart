cb664fda05633e165afcdfdac87fbdbe36a365e8
LesibaFrans <u21566382@tuks.co.za>
2023-07-31 16:59:22 +0200

    Update location_list_widget_test.dart

--- client/test/widgets/location_list_widget_test.dart
+++ client/test/widgets/location_list_widget_test.dart
@@ -1,24 +1,47 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
-import 'package:lottie/lottie.dart';
+import 'package:client/providers/service_provider.dart';
 import 'package:client/widgets/location_list_widget.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:provider/provider.dart';
+import 'package:mockito/mockito.dart';
+
+// Create a mock for ServiceProvider to simulate its behavior during testing
 
 void main() {
-  testWidgets('Test LocationListWidget with empty locationList', (WidgetTester tester) async {
-    await tester.pumpWidget(MaterialApp(home: LocationListWidget()));
-
-    // Verify that the "No Saved Locations" text is displayed
-    expect(find.text('No Saved Locations'), findsOneWidget);
-
-    // Verify that the Lottie animation is displayed
-    expect(find.byType(Lottie), findsOneWidget);
+  setUp(() async{
+    await dotenv.load(fileName: "assets/.env");
   });
+  // Test the LocationListWidget
+  testWidgets('LocationListWidget Test', (WidgetTester tester) async {
+    TestWidgetsFlutterBinding.ensureInitialized();
+    // Create a mock instance of the ServiceProvider
+    final serviceProvider = ServiceProvider();
 
-  testWidgets('Test LocationListWidget delete location', (WidgetTester tester) async {
-    // Create a mock list of locations
+    // Prepare some dummy location list data
+    final locationList = [
+      ['Location 1', 'lat1', 'lng1'],
+      ['Location 2', 'lat2', 'lng2'],
+      ['Location 3', 'lat3', 'lng3'],
+    ];
+    
+    await tester.pumpWidget(
+      MultiProvider(
+        providers: [
+          ChangeNotifierProvider<ServiceProvider>.value(
+            value: serviceProvider,
+          ),
+        ],
+        child: MaterialApp(
+          home: Scaffold(
+            body: LocationListWidget(),
+          ),
+        ),
+      ),
+    );
 
-    await tester.pumpWidget(MaterialApp(home: LocationListWidget()));
-    await tester.pump();
-     expect(find.byType(ListTile), findsAtLeastNWidgets(0));
+    // Test if the LocationListWidget is rendered on the screen
+    expect(find.byType(LocationListWidget), findsOneWidget);
+
   });
 }
