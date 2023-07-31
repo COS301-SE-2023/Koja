15a1b11c97f357b2b05f9076b453915ab9b9efe2
MatjereJ <matjere274@gmail.com>
2023-07-31 10:48:52 +0200

    userController Integration test

--- src/test/kotlin/com/teamcaffiene/koja/controller/UserControllerIntergrationTest.kt
+++ src/test/kotlin/com/teamcaffiene/koja/controller/UserControllerIntergrationTest.kt
@@ -1,9 +1,12 @@
 package com.teamcaffiene.koja.service
 
 import com.teamcaffeine.koja.constants.HeaderConstant
+import com.teamcaffeine.koja.controller.TokenManagerController
+import com.teamcaffeine.koja.controller.TokenRequest
 import com.teamcaffeine.koja.controller.UserController
+import com.teamcaffeine.koja.dto.JWTAuthDetailsDTO
 import com.teamcaffeine.koja.dto.JWTFunctionality
-import com.teamcaffeine.koja.repository.UserAccountRepository
+import com.teamcaffeine.koja.enums.AuthProviderEnum
 import com.teamcaffeine.koja.repository.UserRepository
 import com.teamcaffeine.koja.service.UserCalendarService
 import io.github.cdimascio.dotenv.Dotenv
@@ -12,20 +15,18 @@ import org.junit.jupiter.api.Test
 import org.mockito.Mock
 import org.mockito.MockitoAnnotations
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.http.MediaType
+import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers
-import org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup
 
+@WebMvcTest(UserController::class)
+@ContextConfiguration(classes = [UserCalendarService::class])
 class UserControllerIntergrationTest {
-
-    private lateinit var userController: UserController
     private lateinit var dotenv: Dotenv
-
-    @Mock
-    private lateinit var userAccountRepository: UserAccountRepository
 
     @Mock
     private lateinit var jwtFunctionality: JWTFunctionality
@@ -44,8 +45,6 @@ class UserControllerIntergrationTest {
         MockitoAnnotations.openMocks(this)
         importEnvironmentVariables()
         userCalendarService = UserCalendarService(userRepository, jwtFunctionality)
-        mockMvc = standaloneSetup(userCalendarService).build()
-        userController = UserController(userAccountRepository, userCalendarService)
     }
 
     private fun importEnvironmentVariables() {
@@ -75,7 +74,9 @@ class UserControllerIntergrationTest {
     @Test
     fun `test removeTimeBoundary with valid parameters`() {
         // Mock request parameters
-        val token = "your_token_here"
+        val jwtList = listOf<JWTAuthDetailsDTO>()
+        val tokenRequest = TokenRequest(jwtList, AuthProviderEnum.GOOGLE, 5)
+        val token = TokenManagerController.createToken(tokenRequest)
         val name = "Boundary1"
 
         // Mock the userCalendarService.removeTimeBoundary method
@@ -94,6 +95,26 @@ class UserControllerIntergrationTest {
     }
 
     @Test
+    fun `test removeTimeBoundary with invalid token`() {
+        // Mock request parameters with a missing token
+        val token = "your_token_here"
+        val name = "Boundary1"
+
+        // Mock the userCalendarService.removeTimeBoundary method
+        // whenever(userCalendarService.removeTimeBoundary(token, name)).thenReturn(true)
+
+        // Perform the HTTP POST request
+        mockMvc.perform(
+            MockMvcRequestBuilders.post("/api/v1/user/removeTimeBoundary")
+                .header(HeaderConstant.AUTHORISATION, token)
+                .param("name", name)
+                .contentType(MediaType.APPLICATION_JSON),
+        )
+            // Validate the response
+            .andExpect(MockMvcResultMatchers.status().isForbidden)
+    }
+
+    @Test
     fun `test removeTimeBoundary with missing token`() {
         // Mock request parameters with a missing token
         val name = "Boundary1"
@@ -105,13 +126,38 @@ class UserControllerIntergrationTest {
                 .contentType(MediaType.APPLICATION_JSON),
         )
             // Validate the response
-            .andExpect(MockMvcResultMatchers.status().isNotFound)
+            .andExpect(MockMvcResultMatchers.status().isForbidden)
+    }
+
+    @Test
+    fun `test removeTimeBoundary with valid token and non-existent user`() {
+        // Mock request parameters
+        val name = "Boundary1"
+        val jwtList = listOf<JWTAuthDetailsDTO>()
+        val tokenRequest = TokenRequest(jwtList, AuthProviderEnum.GOOGLE, 5)
+        val token = TokenManagerController.createToken(tokenRequest)
+
+        // Mock the userCalendarService.removeTimeBoundary method to return false for invalid name
+        // whenever(userCalendarService.removeTimeBoundary(token, name)).thenReturn(false)
+
+        // Perform the HTTP POST request
+        mockMvc.perform(
+            MockMvcRequestBuilders.post("/api/v1/user/removeTimeBoundary")
+                .header(HeaderConstant.AUTHORISATION, token)
+                .param("name", name)
+                .contentType(MediaType.APPLICATION_JSON),
+        )
+            // Validate the response
+            .andExpect(MockMvcResultMatchers.status().isBadRequest)
+        // .andExpect(MockMvcResultMatchers.content().string("Something went wrong"))
     }
 
     @Test
     fun `test removeTimeBoundary with invalid name`() {
         // Mock request parameters
-        val token = "your_token_here"
+        val jwtList = listOf<JWTAuthDetailsDTO>()
+        val tokenRequest = TokenRequest(jwtList, AuthProviderEnum.GOOGLE, 5)
+        val token = TokenManagerController.createToken(tokenRequest)
         val name = null
 
         // Mock the userCalendarService.removeTimeBoundary method to return false for invalid name
