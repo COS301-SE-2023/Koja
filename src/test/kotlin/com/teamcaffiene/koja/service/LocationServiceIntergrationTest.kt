696b68af017511111173bc8cce0b5a855920c8fe
MatjereJ <matjere274@gmail.com>
2023-07-31 19:35:27 +0200

    Location service integration tests

--- src/test/kotlin/com/teamcaffiene/koja/service/LocationServiceIntergrationTest.kt
+++ src/test/kotlin/com/teamcaffiene/koja/service/LocationServiceIntergrationTest.kt
@@ -1,18 +1,27 @@
 
 import com.teamcaffeine.koja.KojaApplication
+import com.teamcaffeine.koja.dto.JWTAuthDetailsDTO
 import com.teamcaffeine.koja.dto.JWTFunctionality
+import com.teamcaffeine.koja.dto.UserJWTTokenDataDTO
+import com.teamcaffeine.koja.entity.User
 import com.teamcaffeine.koja.repository.UserRepository
 import com.teamcaffeine.koja.service.GoogleCalendarAdapterService
 import com.teamcaffeine.koja.service.LocationService
 import io.github.cdimascio.dotenv.Dotenv
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertNull
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
 import org.mockito.Mock
 import org.mockito.MockitoAnnotations
 import org.mockito.kotlin.spy
+import org.mockito.kotlin.whenever
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig
+import java.util.*
 
 @SpringJUnitConfig
 @SpringBootTest(classes = [KojaApplication::class], webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -63,5 +72,62 @@ class LocationServiceIntergrationTest {
         dotenv["API_KEY"]?.let { System.setProperty("API_KEY", it) }
 
         dotenv["KOJA_JWT_SECRET"]?.let { System.setProperty("KOJA_JWT_SECRET", it) }
+    }
+
+    @Test
+    fun testSetHomeLocationWithValidInput() {
+        // Prepare test data
+        val mockUserID = Int.MAX_VALUE
+        val userAccounts = mutableListOf<JWTAuthDetailsDTO>()
+        val mockUserJWTData = UserJWTTokenDataDTO(userAccounts, mockUserID)
+        val jwtToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJlbmNyeXB0ZWQiOiIrT09EZXlqWk1CWjV6b3I1OFRBdVZIT2x3akJvbHY1MGFkTjQ5ajdFMWMweW9TWW9zOTNHMUVWT0ord3Z2dGFLQ1RBbE43TzB1Z3crZm1JUmxGZ25jb3EyUHcxRHc2S2RQMVcydnRFWGdIMlFOTFRLSk1sTGkwOGsrWmtsbzRVM1pTcllJb3hzaFVvSk5YU1lnMmQ2K2tRWUpNUEt3MS9ndi9JWjA4U3BHWnpNM3lJaWFlQUt0UFgwZGVQWUlOSk1ZMm1iTlhnZVVIei9Bb3NtWUthNEY3MmlkSEgra0M4N2x6T1ROcU1mSnVCNHRadE40OVJCVlVxQ0JOQ3VrQzNjOUd6NHB3SHVkVVpnbjVnaXFPaXNDNCtSUXo3Uk9EN2I4R2YzdHpJMDZhR2ZURnZld05QSTFPT3JKb3JELzd2bDVHNEtScU1rZDFJaUlKNmd3SUFZanh5bHh2ck0vUHIvUngyWTdOcmRneTBHNHhXMGZjenkzUDhoQmhsYkNSOGFMVEdZdXF0dVdNOUNTMWJmY2hMS2E4UzBvR1pycVR0ak42QzdBOENEZVNHN29VR1ZKOUViSlBLaDVQMlpxVEpKWFlOYWVUclZEQkMxQkxPQ01JeE9QdU1DTWdreHczNWp2bFkyMmFDcUdFZHRJTy9UOXJIMjl4RWJHQUlXeUQvcjNTaW9KT1d2ZzJvd3B5NExKSitTZWVaVjU4UEtCL2wrVFZzTmZBRzB3Vlk2azMxNFk5R2xTbTROUVcyNFRyeDhja01FdHg1eFE1RHhBS0NOWnBXN2pnPT0iLCJleHAiOjE2ODk3MTY5MDR9.uctphVFxJICf8OexH0ZQHWONI3rTExoyDvdAlMdxMGUQaLjGmONyyt6sGP2wn2DUUtW9M1Mg-kbelZpU-zPgbQ"
+        // Mocking
+        val user = User()
+        val optionalUserValue = Optional.of(user)
+        val homeLocation = "New York"
+        userRepository.save(user)
+        whenever(jwtFunctionality.getUserJWTTokenData(jwtToken)).thenReturn(mockUserJWTData)
+        whenever(userRepository.findById(mockUserID)).thenReturn(optionalUserValue)
+
+
+
+        whenever(jwtFunctionality.getUserJWTTokenData(jwtToken)).thenReturn(mockUserJWTData)
+        // Perform the service function call
+        val result = locationService.setHomeLocation(jwtToken, homeLocation)
+
+        // Assert the result
+        assertEquals(homeLocation, result)
+    }
+
+    // Test with a valid access token but user not found in the repository
+    @Test
+    fun testSetHomeLocationWithUserNotFound() {
+        // Prepare test data
+        val accessToken = "your_valid_access_token"
+        val userId = 1L
+        val homeLocation = "New York"
+
+        // Perform the service function call
+        val result = locationService.setHomeLocation(accessToken, homeLocation)
+
+        // Assert the result is null
+        assertNull(result)
+    }
+
+    // Test with a valid access token but no home location provided
+    @Test
+    fun testSetHomeLocationWithNoHomeLocation() {
+        // Prepare test data
+        val accessToken = "your_valid_access_token"
+        val userId = 1L
+
+        val user = User()
+        userRepository.save(user)
+
+        // Perform the service function call with null home location
+        val result = locationService.setHomeLocation(accessToken, null)
+
+        // Assert the result is null (no change in home location)
+        assertNull(result)
     }
 }
