e3c8d8a858c8d9990deb9722560d2ec1e5054c2d
MatjereJ <matjere274@gmail.com>
2023-07-31 13:00:53 +0200

    userController Integration test

--- src/test/kotlin/com/teamcaffiene/koja/controller/UserControllerIntergrationTest.kt
+++ src/test/kotlin/com/teamcaffiene/koja/controller/UserControllerIntergrationTest.kt
@@ -6,6 +6,8 @@ import com.teamcaffeine.koja.controller.TokenRequest
 import com.teamcaffeine.koja.controller.UserController
 import com.teamcaffeine.koja.dto.JWTAuthDetailsDTO
 import com.teamcaffeine.koja.dto.JWTFunctionality
+import com.teamcaffeine.koja.dto.JWTGoogleDTO
+import com.teamcaffeine.koja.dto.UserJWTTokenDataDTO
 import com.teamcaffeine.koja.enums.AuthProviderEnum
 import com.teamcaffeine.koja.repository.UserRepository
 import com.teamcaffeine.koja.service.UserCalendarService
@@ -71,14 +73,20 @@ class UserControllerIntergrationTest {
         dotenv["KOJA_JWT_SECRET"]?.let { System.setProperty("KOJA_JWT_SECRET", it) }
     }
 
-    @Test
     fun `test removeTimeBoundary with valid parameters`() {
         // Mock request parameters
-        val jwtList = listOf<JWTAuthDetailsDTO>()
-        val tokenRequest = TokenRequest(jwtList, AuthProviderEnum.GOOGLE, 5)
-        val token = TokenManagerController.createToken(tokenRequest)
-        val name = "Boundary1"
-
+        val mockUserID = Int.MAX_VALUE
+        val userAccounts = mutableListOf<JWTAuthDetailsDTO>()
+        val mockUserJWTData = UserJWTTokenDataDTO(userAccounts, mockUserID)
+        val authDetails = JWTGoogleDTO("access", "refresh", 60 * 60)
+        val token = TokenManagerController.createToken(
+            TokenRequest(
+                arrayListOf(authDetails),
+                AuthProviderEnum.GOOGLE,
+                mockUserID,
+            ),
+        )
+        val name = "boundary1"
         // Mock the userCalendarService.removeTimeBoundary method
         // whenever(userCalendarService.removeTimeBoundary(token, name)).thenReturn(true)
 
@@ -132,11 +140,12 @@ class UserControllerIntergrationTest {
     @Test
     fun `test removeTimeBoundary with valid token and non-existent user`() {
         // Mock request parameters
-        val name = "Boundary1"
-        val jwtList = listOf<JWTAuthDetailsDTO>()
-        val tokenRequest = TokenRequest(jwtList, AuthProviderEnum.GOOGLE, 5)
-        val token = TokenManagerController.createToken(tokenRequest)
-
+        val mockUserID = Int.MAX_VALUE
+        val userAccounts = mutableListOf<JWTAuthDetailsDTO>()
+        val mockUserJWTData = UserJWTTokenDataDTO(userAccounts, mockUserID)
+        val authDetails = JWTGoogleDTO("access", "refresh", 60 * 60)
+        val token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJlbmNyeXB0ZWQiOiIrT09EZXlqWk1CWjV6b3I1OFRBdVZIT2x3akJvbHY1MGFkTjQ5ajdFMWMweW9TWW9zOTNHMUVWT0ord3Z2dGFLa3pON1UvWDREMjNhY0J0VkxGbG5hM3BiVVVjTjBzVlhrb0F4Ym9VMS9zWDlaMS8rTk1wZVVsUThlNTVSUUNYNnY0WUw0SnR5azFCQUxMS2E3TnAxbDUxTEc0cnZoM0NWdmM4ZGdncXRqcTltRW5LTW1ReGRkNUlwTjN6V1V1aE1XVHF6Y0h1ZkVUUVRCQXJrS0hEZzdLdUg2STRkck51b0U3NDJxSTBtQXRhL3NpZ1IzSXFuRmlJWHpvUFBKejNqc0E2aFBSc1V6WE95YXhSZXIyMDBIT2VDRjJYc1VMUjVCMjJmakpBR21QK282Zkt4NXJuZElRcWx1RWdqTXlhM3VZcHpmUWduMHg2NzhNd0JUZTF5UHl2OE14S05OKzJvaWJqWXFBckxOcDRvTitDK3JJMEhHYWVnQ241SmRlRlh5Yng3S0lGYzR0V0ZyOWx1bEJtWjhocjBFbVV4cXVmSHpFL0pFa2ZzV0ZNUVhDYlFnNHdWQVdPT0FSWWVjeW96NE10TGNtbXFSWUJ3cm9yTkhHTCsrcUx5WUExeUZxNjI4TFNiMHdibVpEc1pXRVNMQys0dDhzWGtPZTdpeXJyV2J0K2IyVWxMM2w1YnRKcGN4bHgwUXVtQXdnSmNYd0RwZ3k3RUd3aDhHQ1hMVjRuZXljMGxBVThjR3VNNU5FdlE0SEs3N3hmSFh6ZnpiYzFsamZjd3VBPT0iLCJleHAiOjE2OTA4MDIyMDZ9.BsvLvXuHfO_em3HDDJau7Fre8Hef-RxFYXoqcMblmNXdfHMsM_KN6X5FcqES0Dkd43skK4nTdHPP8SYpkrKnPw"
+        val name = "boundary1"
         // Mock the userCalendarService.removeTimeBoundary method to return false for invalid name
         // whenever(userCalendarService.removeTimeBoundary(token, name)).thenReturn(false)
 
@@ -155,11 +164,18 @@ class UserControllerIntergrationTest {
     @Test
     fun `test removeTimeBoundary with invalid name`() {
         // Mock request parameters
-        val jwtList = listOf<JWTAuthDetailsDTO>()
-        val tokenRequest = TokenRequest(jwtList, AuthProviderEnum.GOOGLE, 5)
-        val token = TokenManagerController.createToken(tokenRequest)
-        val name = null
-
+        val mockUserID = Int.MAX_VALUE
+        val userAccounts = mutableListOf<JWTAuthDetailsDTO>()
+        val mockUserJWTData = UserJWTTokenDataDTO(userAccounts, mockUserID)
+        val authDetails = JWTGoogleDTO("access", "refresh", 60 * 60)
+        val token = TokenManagerController.createToken(
+            TokenRequest(
+                arrayListOf(authDetails),
+                AuthProviderEnum.GOOGLE,
+                mockUserID,
+            ),
+        )
+        val name = "boundary1"
         // Mock the userCalendarService.removeTimeBoundary method to return false for invalid name
         // whenever(userCalendarService.removeTimeBoundary(token, name)).thenReturn(false)
 
