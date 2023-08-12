f4743d1491768e8ef0a1e0b5d238f0019d8b0a04
UnarineLeo <unarineleo@gmail.com>
2023-08-12 08:43:04 +0200

    update recurrence UI

--- client/lib/widgets/choose_recurrence_widget.dart
+++ client/lib/widgets/choose_recurrence_widget.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import '../Utils/constants_util.dart';
+import 'package:direct_select_flutter/direct_select_container.dart';
+import 'package:direct_select_flutter/direct_select_item.dart';
+import 'package:direct_select_flutter/direct_select_list.dart';
 
 class ChooseRecurrence extends StatefulWidget {
   final void Function(String category) onRecurrenceSelected;
@@ -18,6 +21,15 @@ class ChooseRecurrenceState extends State<ChooseRecurrence> {
   String selectedEnd = 'EndDate';
   List<String> endingChoice = ['Occurrences(times)', 'EndDate'];
   double intervalValue = 1.0;
+
+  List<String> recurrenceString = ['day(s)', 'week(s)', 'month(s)', 'year(s)'];
+  List<String> intervalString = ['1(One)', '2(Two)', '3(Three)', '4(Four)', '5(Five)', '6(Six)', '7(Seven)', '8(Eight)', '9(Nine)', '10(Ten)',
+    '11(Eleven)', '12(Twelve)', '13(Thirteen)', '14(Fourteen)', '15(Fifteen)', '16(Sixteen)', '17(Seventeen)', '18(Eighteen)', '19(Nineteen)', '20(Twenty)',
+    '21(Twenty-One)', '22(Twenty-Two)', '23(Twenty-Three)', '24(Twenty-Four)', '25(Twenty-Five)', '26(Twenty-Six)', '27(Twenty-Seven)', '28(Twenty-Eight)', '29(Twenty-Nine)', '30(Thirty)'];
+
+  int selectedInterval = 0;
+  int selectedRecurrence = 0;
+
 
   @override
   Widget build(BuildContext context) {
@@ -106,6 +118,19 @@ class ChooseRecurrenceState extends State<ChooseRecurrence> {
                               )
                             ],
                           ),
+                          content: Row(
+                            children: 
+                            [
+                              Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Container(
+                                  
+                                ),
+                              ),
+                              Spacer(),
+                              Container()
+                            ],
+                          )
                           
                         ),
                       );
