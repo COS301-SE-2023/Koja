b813b33847933bf18dba0cc4a295f8bfc26b35e8
UnarineLeo <unarineleo@gmail.com>
2023-08-16 03:30:09 +0200

    add selection boxes

--- client/lib/widgets/choose_recurrence_widget.dart
+++ client/lib/widgets/choose_recurrence_widget.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
 import '../Utils/constants_util.dart';
-import 'package:direct_select_flutter/direct_select_container.dart';
-import 'package:direct_select_flutter/direct_select_item.dart';
-import 'package:direct_select_flutter/direct_select_list.dart';
 
 class ChooseRecurrence extends StatefulWidget {
   final void Function(String category) onRecurrenceSelected;
@@ -22,13 +19,13 @@ class ChooseRecurrenceState extends State<ChooseRecurrence> {
   List<String> endingChoice = ['Occurrences(times)', 'EndDate'];
   double intervalValue = 1.0;
 
-  List<String> recurrenceString = ['day(s)', 'week(s)', 'month(s)', 'year(s)'];
-  List<String> intervalString = ['1(One)', '2(Two)', '3(Three)', '4(Four)', '5(Five)', '6(Six)', '7(Seven)', '8(Eight)', '9(Nine)', '10(Ten)',
+  static List<String> recurrenceString = ['day(s)', 'week(s)', 'month(s)', 'year(s)'];
+  static List<String> intervalString = ['1(One)', '2(Two)', '3(Three)', '4(Four)', '5(Five)', '6(Six)', '7(Seven)', '8(Eight)', '9(Nine)', '10(Ten)',
     '11(Eleven)', '12(Twelve)', '13(Thirteen)', '14(Fourteen)', '15(Fifteen)', '16(Sixteen)', '17(Seventeen)', '18(Eighteen)', '19(Nineteen)', '20(Twenty)',
     '21(Twenty-One)', '22(Twenty-Two)', '23(Twenty-Three)', '24(Twenty-Four)', '25(Twenty-Five)', '26(Twenty-Six)', '27(Twenty-Seven)', '28(Twenty-Eight)', '29(Twenty-Nine)', '30(Thirty)'];
 
-  int selectedInterval = 0;
-  int selectedRecurrence = 0;
+  String selectedInterval = recurrenceString[0];
+  String selectedRecurrence = intervalString[0];
 
 
   @override
@@ -76,7 +73,7 @@ class ChooseRecurrenceState extends State<ChooseRecurrence> {
                     builder: (BuildContext context) {
                       return Container(
                         height: MediaQuery.of(context).size.height * 0.8,
-                        width: MediaQuery.of(context).size.width * 0.95, 
+                        width: MediaQuery.of(context).size.width * 0.8,
                         child: AlertDialog(
                           backgroundColor: Colors.grey[100],
                           contentPadding: const EdgeInsets.all(16),
@@ -118,17 +115,92 @@ class ChooseRecurrenceState extends State<ChooseRecurrence> {
                               )
                             ],
                           ),
-                          content: Row(
-                            children: 
-                            [
-                              Padding(
-                                padding: const EdgeInsets.all(8.0),
-                                child: Container(
-                                  
+                          content: Column(
+                            children: [
+                              Text(
+                                'Repeats Every',
+                                style: TextStyle(
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.w500,
+                                  fontSize: 17,
                                 ),
                               ),
-                              Spacer(),
-                              Container()
+                              Row(
+                                children: 
+                                [
+                                  Expanded(
+                                    child: DropdownButtonFormField<String>(
+                                      value: selectedRecurrence,
+                                      items: intervalString.map<DropdownMenuItem<String>>((String value) {
+                                        return DropdownMenuItem<String>(
+                                          value: value,
+                                          child: Text(value),
+                                        );
+                                      }).toList(),
+                                      borderRadius: BorderRadius.circular(10),
+                                      decoration: InputDecoration(
+                                        label: Text(
+                                          'End',
+                                          style: TextStyle(
+                                            color: Colors.black,
+                                            fontWeight: FontWeight.w500,
+                                            fontSize: 17
+                                          ),
+                                        ),
+                                        border: OutlineInputBorder(
+                                          borderRadius: BorderRadius.circular(10),
+                                        ),
+                                        focusedBorder: OutlineInputBorder(
+                                          borderSide: BorderSide(color: Colors.black, width: 2.0),
+                                        ),
+                                      ),  
+                                      onChanged: (String? newValue) {
+                                        if (newValue != null) {
+                                          setState(() {
+                                            selectedEnd = newValue;
+                                          });
+                                        }
+                                      },
+                                    ),
+                                  ),
+                                  // Spacer(),
+                                  Expanded(
+                                    child: DropdownButtonFormField<String>(
+                                      value: selectedInterval,
+                                      items: recurrenceString.map<DropdownMenuItem<String>>((String value) {
+                                        return DropdownMenuItem<String>(
+                                          value: value,
+                                          child: Text(value),
+                                        );
+                                      }).toList(),
+                                      borderRadius: BorderRadius.circular(10),
+                                      decoration: InputDecoration(
+                                        label: Text(
+                                          'Interval',
+                                          style: TextStyle(
+                                            color: Colors.black,
+                                            fontWeight: FontWeight.w500,
+                                            fontSize: 17
+                                          ),
+                                        ),
+                                        border: OutlineInputBorder(
+                                          borderRadius: BorderRadius.circular(10),
+                                        ),
+                                        focusedBorder: OutlineInputBorder(
+                                          borderSide: BorderSide(color: Colors.black, width: 2.0),
+                                        ),
+                                      ),  
+                                      onChanged: (String? newValue) {
+                                        if (newValue != null) {
+                                          setState(() {
+                                            selectedInterval = newValue;
+                                          });
+                                        }
+                                      },
+                                    ), 
+                                  )
+                                ],
+                              ),
                             ],
                           )
                           
