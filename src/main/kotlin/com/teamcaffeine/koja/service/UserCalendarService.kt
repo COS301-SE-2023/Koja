b89dc7ecf805aba2bed6406a68988b2e37ae3279
U18034332 <u18034332@tuks.co.za>
2023-08-16 08:48:32 +0200

    Updated scheduler to support event priorities

--- src/main/kotlin/com/teamcaffeine/koja/service/UserCalendarService.kt
+++ src/main/kotlin/com/teamcaffeine/koja/service/UserCalendarService.kt
@@ -200,7 +200,11 @@ class UserCalendarService(
             val newEventDuration =
                 (eventDTO.getDurationInSeconds() + travelDuration) * 1000L // Multiply by 1000 to convert to milliseconds
             eventDTO.setDuration(newEventDuration)
-
+            if (eventDTO.getPriority() == 3) {
+                val (earliestSlotStartTime, earliestSlotEndTime) = findEarliestTimeSlot(userEvents, eventDTO)
+                eventDTO.setStartTime(earliestSlotStartTime)
+                eventDTO.setEndTime(earliestSlotEndTime)
+            }
             val (earliestSlotStartTime, earliestSlotEndTime) = findEarliestTimeSlot(userEvents, eventDTO)
             eventDTO.setStartTime(earliestSlotStartTime)
             eventDTO.setEndTime(earliestSlotEndTime)
