99713f6a69f67b10c5451faeac3c36007ddd7e49
Marthinus Ettienne van Zyl - u19012366 <u19012366@tuks.co.za>
2023-08-19 20:47:02 +0200

    Update Controller to require Secret

--- src/main/kotlin/com/teamcaffeine/koja/controller/AIDataController.kt
+++ src/main/kotlin/com/teamcaffeine/koja/controller/AIDataController.kt
@@ -11,6 +11,7 @@ import com.teamcaffeine.koja.constants.ResponseConstant
 import com.teamcaffeine.koja.repository.UserAccountRepository
 import com.teamcaffeine.koja.service.AIUserDataService
 import com.teamcaffeine.koja.service.UserCalendarService
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestHeader
@@ -27,8 +28,8 @@ class AIDataController(private val userAccountRepository: UserAccountRepository,
     fun getUserEventData(@RequestHeader(HeaderConstant.AUTHORISATION) token: String?): ResponseEntity<out Any> {
         return if (token == null) {
             ResponseEntity.badRequest().body(ResponseConstant.REQUIRED_PARAMETERS_NOT_SET)
-//        } else if (token != System.getProperty("KOJA_JWT_SECRET")) {
-//            ResponseEntity<String>("Unauthorized", HttpStatus.UNAUTHORIZED)
+        } else if (token != System.getProperty("KOJA_AI_SECRET")) {
+            ResponseEntity<String>("Unauthorized", HttpStatus.UNAUTHORIZED)
         } else {
             val gsonBuilder = GsonBuilder()
             gsonBuilder.registerTypeAdapter(
