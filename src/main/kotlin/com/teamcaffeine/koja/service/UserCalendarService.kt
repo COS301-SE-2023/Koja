fbba66049d4aad02028c08f2e4e3e3bd51ea5e70
U18034332 <u18034332@tuks.co.za>
2023-08-29 09:34:47 +0200

    Minor changes to event priority

--- src/main/kotlin/com/teamcaffeine/koja/service/UserCalendarService.kt
+++ src/main/kotlin/com/teamcaffeine/koja/service/UserCalendarService.kt
@@ -503,7 +503,8 @@ class UserCalendarService(
         return todayEvents.values.toList()
     }
 
-    fun reassignEarliestTimes(events: List<UserEventDTO>): List<UserEventDTO> {
+    fun reassignEarliestTimes(token: String): List<UserEventDTO> {
+        val events = getAllUserDynamicEvents(token)
         val sortedEvents = events
             .filter { it.isDynamic() }
             .sortedBy { it.getPriority() }
@@ -515,12 +516,10 @@ class UserCalendarService(
             val eventDateTime = event.getStartTime()
             if (eventDateTime.isAfter(currentDateTime)) {
                 currentDateTime = eventDateTime
+                event.setStartTime(currentDateTime)
             }
-            assignedEvents.add(event.getStartTime(currentDateTime))
+            assignedEvents.add(event)
         }
-
         return assignedEvents
     }
-
-
 }
