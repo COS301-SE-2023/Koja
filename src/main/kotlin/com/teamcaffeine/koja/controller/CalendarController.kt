7f91ce9e187b76afdeebdfdbd74483ea82061c50
Elias Makhonjwa <126951032+U18034332@users.noreply.github.com>
2023-09-01 10:15:13 +0200

    Minor changes to recureence

--- src/main/kotlin/com/teamcaffeine/koja/controller/CalendarController.kt
+++ src/main/kotlin/com/teamcaffeine/koja/controller/CalendarController.kt
@@ -36,7 +36,7 @@ class CalendarController(private val userCalendar: UserCalendarService) {
                     ResponseEntity.badRequest()
                         .body(ResponseConstant.EVENT_CREATION_FAILED_COULD_NOT_FIT)
                 }
-                return ResponseEntity.internalServerError().body(e.message)
+                return ResponseEntity.internalServerError().body(ResponseConstant.EVENT_CREATION_FAILED_INTERNAL_ERROR)
             }
             ResponseEntity.ok(ResponseConstant.EVENT_CREATED)
         }
